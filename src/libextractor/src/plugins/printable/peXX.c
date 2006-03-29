b68ef7ddab168c2559768b3a88a882c9420003cf
Jamie McCracken <jamiemcc@src.gnome.org>
2006-03-29 11:47:19 +0000

    *** empty log message ***

--- src/libextractor/src/plugins/printable/peXX.c
+++
@@ -1,5 +0,0 @@
-#include "platform.h"
-#define FILTER_NAME libextractor_printable_XX_filter
-#define FILTER_INIT_NAME libextractor_printable_XX_filter_init
-#define EXTRACT_NAME libextractor_printable_XX_extract
-#include "printableextractor.h"
