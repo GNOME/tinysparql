b7992cf3046fd83fb04ae6985e4ae3c0c8d2212e
Jamie McCracken <jamiemcc@src.gnome.org>
2006-07-17 16:53:02 +0000

    *** empty log message ***

---
+++ src/trackerd/tracker-dbus-files.h
@@ -0,0 +1,40 @@
+/* Tracker
+ * Copyright (C) 2005, Mr Jamie McCracken (jamiemcc@gnome.org)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "tracker-dbus.h"
+
+void		tracker_dbus_method_files_create		(DBusRec *rec);
+void		tracker_dbus_method_files_delete		(DBusRec *rec);
+void		tracker_dbus_method_files_get_text_contents	(DBusRec *rec);
+void		tracker_dbus_method_files_search_text_contents	(DBusRec *rec);
+void		tracker_dbus_method_files_get_mtime		(DBusRec *rec);
+void		tracker_dbus_method_files_get_by_service_type	(DBusRec *rec);
+void		tracker_dbus_method_files_get_by_mime_type	(DBusRec *rec);
+void		tracker_dbus_method_files_get_by_mime_type_vfs	(DBusRec *rec);
+
+void		tracker_dbus_method_files_get_metadata_for_files_in_folder 	(DBusRec *rec);
+void		tracker_dbus_method_files_search_by_text_mime			(DBusRec *rec);
+void		tracker_dbus_method_files_search_by_text_location		(DBusRec *rec);
+void		tracker_dbus_method_files_search_by_text_mime_location		(DBusRec *rec);
+
+
+
+
+
+
---
+++ src/trackerd/tracker-dbus-search.h
@@ -0,0 +1,26 @@
+/* Tracker
+ * Copyright (C) 2005, Mr Jamie McCracken (jamiemcc@gnome.org)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "tracker-dbus.h"
+
+void	tracker_dbus_method_search_text 			(DBusRec *rec);
+void	tracker_dbus_method_search_files_by_text 		(DBusRec *rec);
+void	tracker_dbus_method_search_metadata 			(DBusRec *rec);
+void	tracker_dbus_method_search_matching_fields 		(DBusRec *rec);
+void	tracker_dbus_method_search_query 			(DBusRec *rec);
