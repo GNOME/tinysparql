79b9fa6ee5e4723f8b7a53845727d0f0284655ee
Jamie McCracken <jamiemcc@src.gnome.org>
2006-10-03 11:34:48 +0000

    ioprio support

---
+++ src/trackerd/tracker-ioprio.c
@@ -0,0 +1,58 @@
+/* Tracker
+ * io priority
+ * Copyright (C) 2006, Anders Aagaard
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+ 
+#include <stdio.h>
+#include <errno.h>
+#include <glib/gstdio.h>
+#include <tracker-utils.h>
+#include <linux/unistd.h> 
+#include "tracker-ioprio.h"
+
+#ifdef IOPRIO_SUPPORT
+
+enum {
+	IOPRIO_CLASS_NONE,
+	IOPRIO_CLASS_RT,
+	IOPRIO_CLASS_BE,
+	IOPRIO_CLASS_IDLE,
+};
+
+enum {
+	IOPRIO_WHO_PROCESS = 1,
+	IOPRIO_WHO_PGRP,
+	IOPRIO_WHO_USER,
+};
+
+#define IOPRIO_CLASS_SHIFT	13
+
+_syscall2(int, ioprio_get, int, which, int, who); 
+_syscall3(int, ioprio_set, int, which, int, who, int, ioprio); 
+
+void ioprio()
+{
+	int ioprio = 7, ioprio_class = IOPRIO_CLASS_BE;
+	tracker_log ("Setting ioprio best effort.");
+	
+	if (ioprio_set(IOPRIO_WHO_PROCESS,0,ioprio | ioprio_class << IOPRIO_CLASS_SHIFT) == -1) {
+		perror ("ioprio_set returns error.");
+	}
+}
+
+#endif
---
+++ src/trackerd/tracker-ioprio.h
@@ -0,0 +1,30 @@
+/* Tracker
+ * io priority
+ * Copyright (C) 2006, Anders Aagaard
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <linux/unistd.h> 
+
+#ifndef TRACKER_IOPRIO_H
+#define TRACKER_IOPRIO_H
+
+#ifdef IOPRIO_SUPPORT
+	void ioprio();
+#endif
+
+#endif
