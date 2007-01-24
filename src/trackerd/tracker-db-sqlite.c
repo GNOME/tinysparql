e6c9f3ca85c29034856043dcd1c091cd7fcd2563
Jamie McCracken <jamiemcc@src.gnome.org>
2007-01-24 13:21:26 +0000

    added magic.h check to configure.ac
    svn path=/trunk/; revision=423

--- src/trackerd/tracker-db-sqlite.c
+++ src/trackerd/tracker-db-sqlite.c
@@ -2406,7 +2406,7 @@ tracker_db_insert_embedded_metadata (DBConnection *db_con, const char *service, 
 void
 tracker_db_update_multple_metdata (DBConnection *db_con, const char *service, const char *id, const char *key, char **values, gboolean generate_display_metadata, gboolean embedded) 
 {
-
+	
 
 }
 
