41f565b7a72bb4ad1af454d684d083a0c59963c5
Luca Ferretti <elle.uca@libero.it>
2007-07-02 20:17:55 +0000

    Add -DTRACKER_LOCALEDIR in INCLUDES
    2007-07-02  Luca Ferretti  <elle.uca@libero.it>
    
    	* src/tracker-preferences/Makefile.am:
    	Add -DTRACKER_LOCALEDIR in INCLUDES
    	
    	* src/tracker-preferences/tracker-preferences-main.c: (main):
    	Enable localization at runtime.
    
    
    svn path=/trunk/; revision=621

--- src/tracker-preferences/tracker-preferences-main.c
+++ src/tracker-preferences/tracker-preferences-main.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 
+#include <glib/gi18n.h>
 #include <gtk/gtk.h>
 
 #include "tracker-preferences.h"
+#include "config.h"
 
 gboolean
 terminate (gpointer data)
@@ -15,6 +17,10 @@ main (int argc, char *argv[])
 {
 	GMainLoop *loop = NULL;
 	TrackerPreferences *preferences = NULL;
+        
+        bindtextdomain (GETTEXT_PACKAGE, TRACKER_LOCALEDIR);
+        bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+        textdomain (GETTEXT_PACKAGE);
 
 	gtk_init (&argc, &argv);
 	preferences = tracker_preferences_new ();
