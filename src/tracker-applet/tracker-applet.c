1e6df64d1ea25cd43743d34e827eac55395f5048
Jaap Haitsma <jaap@haitsma.org>
2008-01-05 03:25:21 +0000

    Remove *-private.h files. Just put these declarations in the respective
    2008-01-05  Jaap Haitsma  <jaap@haitsma.org>
    
    	* src/tracker-applet/Makefile.am:
    	* src/tracker-applet/tracker-applet-private.h:
    	* src/tracker-applet/tracker-applet.c:
    	* src/tracker-preferences/Makefile.am:
    	* src/tracker-preferences/tracker-configuration-private.h:
    	* src/tracker-preferences/tracker-configuration.c:
    	* src/tracker-preferences/tracker-preferences-private.h:
    	* src/tracker-preferences/tracker-preferences.c:
    	Remove *-private.h files. Just put these declarations in the
    	respective *.c files
    
    svn path=/trunk/; revision=1093

--- src/tracker-applet/tracker-applet.c
+++ src/tracker-applet/tracker-applet.c
@@ -1,3 +1,5 @@
+#include "config.h"
+
 
 #include <stdlib.h>
 #include <strings.h>
@@ -6,11 +8,10 @@
 #include <gdk/gdkkeysyms.h>
 #include <gtk/gtk.h>
 #include <libnotify/notify.h>
-
-#include "config.h"
+#include <dbus/dbus-glib.h>
+#include <dbus/dbus-glib-bindings.h>
 
 #include "tracker-applet.h"
-#include "tracker-applet-private.h"
 #include "tracker.h"
 #include "tracker-applet-marshallers.h"
 
@@ -23,6 +24,61 @@
 #define DBUS_SERVICE_TRACKER    "org.freedesktop.Tracker"
 #define DBUS_PATH_TRACKER       "/org/freedesktop/tracker"
 #define DBUS_INTERFACE_TRACKER  "org.freedesktop.Tracker"
+
+#define TRAY_ICON_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE((obj), TYPE_TRAY_ICON, TrayIconPrivate))
+
+#define TRACKER_ICON            "tracker-applet-default.png"
+#define TRACKER_ICON_PAUSED     "tracker-applet-paused.png"
+#define TRACKER_ICON_INDEX1     "tracker-applet-indexing1.png"
+#define TRACKER_ICON_INDEX2     "tracker-applet-indexing2.png"
+
+
+typedef enum {
+	ICON_DEFAULT,
+	ICON_PAUSED,
+	ICON_INDEX1,
+	ICON_INDEX2,
+} IndexIcon;
+
+
+static char *index_icons[4] = {TRACKER_ICON, TRACKER_ICON_PAUSED, TRACKER_ICON_INDEX1, TRACKER_ICON_INDEX2};
+
+
+typedef struct _TrayIconPrivate
+{
+   	GtkStatusIcon 		*icon;
+
+	/* states */
+	gboolean 		indexing;
+	gboolean 		paused;
+	IndexIcon		index_icon;
+	gboolean		animated;
+
+	/* main window */
+   	GtkMenu 		*menu;
+	GtkWindow		*window;
+	GtkWidget 		*search_entry;	
+	GtkWidget 		*status_label;
+	GtkWidget 		*progress_bar;
+	GtkWidget 		*email_progress_bar;
+	GtkWidget 		*uri_label;
+
+	gboolean		initial_index_msg_shown;
+
+	/* tracker connection */
+	TrackerClient		*tracker;
+
+	/* stats window table shown */
+	gboolean		stat_window_active;
+	gboolean		stat_request_pending;
+} TrayIconPrivate;
+
+static void tray_icon_class_init (TrayIconClass *klass);
+static void tray_icon_init (GTypeInstance *instance, gpointer g_class);
+static void tray_icon_clicked (GtkStatusIcon *icon, guint button, guint timestamp, gpointer data);
+static void preferences_menu_activated (GtkMenuItem *item, gpointer data);
+static void statistics_menu_activated (GtkMenuItem *item, gpointer data);
+static void quit_menu_activated (GtkMenuItem *item, gpointer data);
 
 static TrayIcon *main_icon;
 
