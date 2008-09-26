caa9320c8ae7800acf2160701eb9284ac05e4eb9
Martyn James Russell <mr@src.gnome.org>
2008-09-26 10:41:53 +0000

    Removed everything in preparation for bringing in new files
    svn path=/trunk/; revision=2274

--- data/sqlite-tracker-triggers.sql
+++
@@ -1,9 +0,0 @@
-BEGIN TRANSACTION;!
-
-
-CREATE TRIGGER delete_backup_service BEFORE DELETE ON BackupServices 
-BEGIN  
-	DELETE FROM BackupMetaData WHERE ServiceID = old.ID;
-END;!
-
-COMMIT TRANSACTION;!
