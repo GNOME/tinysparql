451bcb905b5c0a70cb1f656936b06184f77fdc43
Philip Van Hoof <pvanhoof@src.gnome.org>
2009-01-20 11:27:44 +0000

    added a sample XSLT Added a sample XSLT
            * utils/services/sample.xsl: added a sample XSLT
            * utils/services/README.example: Added a sample XSLT
    
    
    
    svn path=/trunk/; revision=2808

--- utils/services/service2xml.py
+++ utils/services/service2xml.py
@@ -4,9 +4,10 @@ import sys
 config = ConfigParser.ConfigParser()
 config.readfp(sys.stdin)
 
+print "<service>"
 for section in config.sections():
    splitted = section.split (":")
-   print "<section prefix=\"" + splitted[0] + "\" name=\"" + splitted[1] + "\" fullname=\"" + section + "\">"
+   print " <section prefix=\"" + splitted[0] + "\" name=\"" + splitted[1] + "\" fullname=\"" + section + "\">"
    for name, value in config.items (section):
      vsplit = value.split (":")
      print "\t<item name=\""+ name + "\" fullvalue=\"" + value+ "\">"
@@ -17,4 +18,5 @@ for section in config.sections():
        print "\t\t<prefix />"
        print "\t\t<value>" + value + "</value>"
      print "\t</item>"
-   print "</section>"
+   print " </section>"
+print "</service>"
