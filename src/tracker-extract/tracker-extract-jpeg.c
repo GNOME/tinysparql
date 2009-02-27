88f13841c8d2aaaa9e86bf2d87a7633f3598b9be
Mikael Ottela <mottela@src.gnome.org>
2009-02-27 14:35:34 +0000

    Put back variables needed for exif extraction
    svn path=/trunk/; revision=2990

--- src/tracker-extract/tracker-extract-jpeg.c
+++ src/tracker-extract/tracker-extract-jpeg.c
@@ -302,6 +302,10 @@ extract_jpeg (const gchar *filename,
 	if ((jpeg = fdopen (fd_jpeg, "rb"))) {
 		gchar *str;
 		gsize  len;
+#ifdef HAVE_LIBEXIF
+		gsize  offset;
+		gsize  sublen;
+#endif /* HAVE_LIBEXIF */
 
 		cinfo.err = jpeg_std_error (&tejerr.jpeg);
 		tejerr.jpeg.error_exit = tracker_extract_jpeg_error_exit;
