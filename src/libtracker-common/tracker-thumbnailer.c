e77a9e88ba64c7b22dfbe4deda83b0a158af1577
Philip Van Hoof <philip@codeminded.be>
2009-03-10 13:37:31 +0000

    Using no_reply calls for the DBus methods
    2009-03-10  Philip Van Hoof  <philip@codeminded.be>
    
            * src/libtracker-common/tracker-thumbnailer.c: Using no_reply calls
            for the DBus methods
    
    
    svn path=/trunk/; revision=3041

--- src/libtracker-common/tracker-thumbnailer.c
+++ src/libtracker-common/tracker-thumbnailer.c
@@ -143,70 +143,6 @@ thumbnailer_enabled_cb (GObject    *pspec,
 		 private->service_is_enabled ? "enabled" : "disabled");
 }
 
-static void
-thumbnailer_reply_handle (DBusGProxy     *proxy,
-			  DBusGProxyCall *call,
-			  guint           request_id,
-			  const gchar    *operation)
-{
-	GError *error = NULL;
-	guint	handle;
-
-	/* The point of this is dbus-glib correctness. Answering this
-	 * because this comment used to be the question: what is the
-	 * point of this. It's correct this way because we do
-	 * asynchronous DBus calls using glib-dbus. For asynchronous
-	 * DBus calls it's recommended (if not required for cleaning
-	 * up) to call dbus_g_proxy_end_call.
-	 */
-	dbus_g_proxy_end_call (proxy, call, &error,
-			       G_TYPE_UINT, &handle,
-			       G_TYPE_INVALID);
-
-	if (error) {
-		g_warning ("Thumbnailer %s failed for request ID:%d, %s", 
-			   operation,
-			   request_id,
-			   error->message);
-		g_error_free (error);
-		return;
-	}
-
-	g_debug ("Thumbnailer response received for request ID:%d",
-		 request_id);
-}
-
-static void
-thumbnailer_reply_queue_cb (DBusGProxy     *proxy,
-			    DBusGProxyCall *call,
-			    gpointer	    user_data)
-{
-	thumbnailer_reply_handle (proxy, call, GPOINTER_TO_UINT (user_data), "queue");
-}
-
-static void
-thumbnailer_reply_move_cb (DBusGProxy     *proxy,
-			   DBusGProxyCall *call,
-			   gpointer	   user_data)
-{
-	thumbnailer_reply_handle (proxy, call, GPOINTER_TO_UINT (user_data), "move");
-}
-
-static void
-thumbnailer_reply_delete_cb (DBusGProxy     *proxy,
-			     DBusGProxyCall *call,
-			     gpointer	     user_data)
-{
-	thumbnailer_reply_handle (proxy, call, GPOINTER_TO_UINT (user_data), "delete");
-}
-
-static void
-thumbnailer_reply_cleanup_cb (DBusGProxy     *proxy,
-			      DBusGProxyCall *call,
-			      gpointer	      user_data)
-{
-	thumbnailer_reply_handle (proxy, call, GPOINTER_TO_UINT (user_data), "cleanup");
-}
 
 static gboolean
 thumbnailer_request_timeout_cb (gpointer data)
@@ -226,15 +162,13 @@ thumbnailer_request_timeout_cb (gpointer data)
 		 private->count,
 		 private->request_id);
 	
-	dbus_g_proxy_begin_call (private->requester_proxy,
-				 "Queue",
-				 thumbnailer_reply_queue_cb,
-				 GUINT_TO_POINTER (private->request_id), 
-				 NULL,
-				 G_TYPE_STRV, private->uris,
-				 G_TYPE_STRV, private->mime_types,
-				 G_TYPE_UINT, 0,
-				 G_TYPE_INVALID);
+	dbus_g_proxy_call_no_reply (private->requester_proxy,
+				    "Queue",
+				    G_TYPE_STRV, private->uris,
+				    G_TYPE_STRV, private->mime_types,
+				    G_TYPE_UINT, 0,
+				    G_TYPE_INVALID,
+				    G_TYPE_INVALID);
 	
 	for (i = 0; i <= private->count; i++) {
 		g_free (private->uris[i]);
@@ -391,15 +325,13 @@ tracker_thumbnailer_move (const gchar *from_uri,
 	} else {
 		from[0] = g_strdup (from_uri);
 	}
-	
-	dbus_g_proxy_begin_call (private->requester_proxy,
-				 "Move",
-				 thumbnailer_reply_move_cb,
-				 GUINT_TO_POINTER (private->request_id), 
-				 NULL,
-				 G_TYPE_STRV, from,
-				 G_TYPE_STRV, to,
-				 G_TYPE_INVALID);
+
+	dbus_g_proxy_call_no_reply (private->requester_proxy,
+				    "Move",
+				    G_TYPE_STRV, from,
+				    G_TYPE_STRV, to,
+				    G_TYPE_INVALID,
+				    G_TYPE_INVALID);
 
 	g_free (from[0]);
 	g_free (to[0]);
@@ -446,13 +378,11 @@ tracker_thumbnailer_remove (const gchar *uri,
 		 uri,
 		 private->request_id); 
 	
-	dbus_g_proxy_begin_call (private->requester_proxy,
-				 "Delete",
-				 thumbnailer_reply_delete_cb,
-				 GUINT_TO_POINTER (private->request_id),
-				 NULL,
-				 G_TYPE_STRV, uris,
-				 G_TYPE_INVALID);
+	dbus_g_proxy_call_no_reply (private->requester_proxy,
+				    "Delete",
+				    G_TYPE_STRV, uris,
+				    G_TYPE_INVALID,
+				    G_TYPE_INVALID);
 
 	g_free (uris[0]);
 }
@@ -479,14 +409,12 @@ tracker_thumbnailer_cleanup (const gchar *uri_prefix)
 		 uri_prefix,
 		 private->request_id); 
 
-	dbus_g_proxy_begin_call (private->requester_proxy,
-				 "Cleanup",
-				 thumbnailer_reply_cleanup_cb,
-				 GUINT_TO_POINTER (private->request_id),
-				 NULL,
-				 G_TYPE_STRING, uri_prefix,
-				 G_TYPE_INT64, 0,
-				 G_TYPE_INVALID);
+	dbus_g_proxy_call_no_reply (private->requester_proxy,
+				    "Cleanup",
+				    G_TYPE_STRING, uri_prefix,
+				    G_TYPE_INT64, 0,
+				    G_TYPE_INVALID,
+				    G_TYPE_INVALID);
 }
 
 void
