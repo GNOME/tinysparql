6eac4fb9715a3ed01d6a7e4e36d8c05a76eefd7d
Ivan Frade <ifrade@src.gnome.org>
2009-04-14 11:00:15 +0200

    Add stress test programs

---
+++ tests/estress/mock_engine.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python2.5
+#
+# Random input generator for tracker.
+# Example to implement one data generator.
+#
+# Copyright (C) 2009 Nokia <urho.konttori@nokia.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+# USA
+#
+import gobject
+import getopt, sys
+import options
+
+options = options.parse_options (graphic_mode=False, period=1, msgsize=2, timeout=0)
+
+if options['graphic_mode']:
+    import gtk
+    from abstract_engine import AbstractEngine
+    superKlass = AbstractEngine
+    mainloop = gtk.main
+else:
+    from abstract_text_engine import AbstractTextEngine
+    superKlass = AbstractTextEngine
+    mainloop = gobject.MainLoop().run
+
+class MockEngine (superKlass):
+
+    def __init__ (self, name, period, msgsize, timeout):
+        """
+        self.publicname
+        self.msgsize
+        self.period contains these values for the subclasses
+        """
+        superKlass.__init__ (self, name, period, msgsize, timeout)
+        self.run ()
+        
+    def get_insert_sparql (self):
+        """
+        This method returns an string with the sparQL we will send
+        to tracker.SparqlUpdate method
+        """
+        return "INSERT { put here your triplets }"
+
+    def get_running_label (self):
+        """
+        This method returns the string showing the current status
+        when the engine is running
+        """
+        return "%s sends %s items every %s sec." % (self.publicname,
+                                                          self.msgsize,
+                                                          self.period)
+
+
+if __name__ == "__main__":
+
+    gobject.set_application_name ("Here the title of the window")
+    engine = MockEngine ("My mock stuff",
+                         options['period'],
+                         options['msgsize'],
+                         options['timeout'])
+    mainloop ()
+
---
+++ tests/estress/options.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python2.5
+#
+# Random input generator for tracker.
+#  Command line options parsing
+#
+# Copyright (C) 2009 Nokia <urho.konttori@nokia.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+# USA
+#
+import getopt
+import sys
+
+def usage ():
+    print "Usage:"
+    print "  xxx.py [OPTION...] - Input data into tracker"
+    print ""
+    print "Help Options:"
+    print "  -h, --help             Show help options"
+    print ""
+    print "Application Options:"
+    print "  -g, --graphics         Enable GTK interface"
+    print "  -p, --period=NUM       Time (in sec) between insertion message"
+    print "  -s, --size=NUM         Amount of instances in the message"
+    print "  -t, --timeout=NUM      Switch off the program after NUM seconds"
+    print ""
+
+
+def parse_options (graphic_mode=False, period=1, msgsize=1, timeout=0):
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],
+                                   "gp:s:t:h",
+                                   ["graphics", "period", "size", "timeout", "help"])
+    except getopt.GetoptError, err:
+        # print help information and exit:
+        print str (err) # will print something like "option -a not recognized"
+        usage ()
+        sys.exit (2)
+
+    options = { "graphic_mode" : graphic_mode ,
+                "period"  : period,
+                "msgsize" : msgsize,
+                "timeout" : timeout }
+
+
+    for o, a in opts:
+        if o in ["-g", "--graphics"]:
+            options ['graphic_mode'] = True
+            
+        elif o in ["-p", "--period"]:
+            options ['period'] = int(a)
+            
+        elif o in ["-s", "--size"]:
+            options ['msgsize'] = int (a)
+            
+        elif o in ["-t", "--timeout"]:
+            options ['timeout'] = int (a)
+        elif o in ["-h", "--help"]:
+            usage ()
+            sys.exit (-1)
+        else:
+            usage ()
+            assert False, "unhandled option"
+
+    return options
+
