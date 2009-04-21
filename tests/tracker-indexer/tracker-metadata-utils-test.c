a3a65d676668b69ece92bf2c46fe2be84e0ab2a0
Ivan Frade <ivan.frade@nokia.com>
2009-04-21 17:17:48 +0300

    Add mock test
    A completely empty unit test file was breaking gtester report tool.

--- tests/tracker-indexer/tracker-metadata-utils-test.c
+++ tests/tracker-indexer/tracker-metadata-utils-test.c
@@ -1,6 +1,41 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * Copyright (C) 2008, Nokia (urho.konttori@nokia.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+#include <glib.h>
+
+#include <tracker-test-helpers.h>
+
+void
+test_something () {
+        g_assert_cmpint (1, ==, 1);
+        return;
+}
+
 int
 main (int    argc,
       char **argv)
 {
-	return 0;
+        g_type_init ();
+        g_thread_init (NULL);
+        g_test_init (&argc, &argv, NULL);
+
+        g_test_add_func ("/tracker-indexer/tracker-metadata-utils/something",
+                         test_something);
+	return g_test_run ();
 }
