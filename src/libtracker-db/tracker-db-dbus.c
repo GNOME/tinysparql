5544e53d943725ec12870c955162bb06c6608279
Mikael Ottela <mikael.ottela@ixonos.com>
2009-05-14 08:19:39 +0300

    Do not use default values for numeric null fields
    Instead of setting a default value for numeric fields with null content we
    just return an empty string in this case as well.
    
    Fixes NB#115978

--- src/libtracker-db/tracker-db-dbus.c
+++ src/libtracker-db/tracker-db-dbus.c
@@ -306,7 +306,7 @@ tracker_dbus_query_result_columns_to_strv (TrackerDBResultSet *result_set,
 			g_value_init (&transform, G_TYPE_STRING);
 			
 			_tracker_db_result_set_get_value (result_set, i, &value);
-			if (g_value_transform (&value, &transform)) {
+			if (G_IS_VALUE (&value) && g_value_transform (&value, &transform)) {
 				if (row_counter == 0) {
 					strv[i] = g_value_dup_string (&transform);
 				} else {
@@ -326,8 +326,11 @@ tracker_dbus_query_result_columns_to_strv (TrackerDBResultSet *result_set,
 					}
 					
 				}
+				g_value_unset (&value);
+			} else {
+				strv[i] = g_strdup ("");
 			}
-			g_value_unset (&value);
+			
 			g_value_unset (&transform);
 		}
 
@@ -448,7 +451,7 @@ tracker_dbus_query_result_to_ptr_array (TrackerDBResultSet *result_set)
 
 			_tracker_db_result_set_get_value (result_set, i, &value);
 
-			if (g_value_transform (&value, &transform)) {
+			if (G_IS_VALUE (&value) && g_value_transform (&value, &transform)) {
 				str = g_value_dup_string (&transform);
 			}
 
@@ -458,7 +461,9 @@ tracker_dbus_query_result_to_ptr_array (TrackerDBResultSet *result_set)
 
 			list = g_slist_prepend (list, (gchar*) str);
 
-			g_value_unset (&value);
+			if (G_IS_VALUE (&value)) {
+				g_value_unset (&value);
+			}
 			g_value_unset (&transform);
 		}
 
