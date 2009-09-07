1739c6d5d9fdda12a23e73654c9e227f05f28088
Jürg Billeter <j@bitron.ch>
2009-09-07 11:13:13 +0200

    Drop unused options from common.db

--- data/db/sqlite-tracker.sql
+++ data/db/sqlite-tracker.sql
@@ -4,13 +4,7 @@ CREATE TABLE Options (
 	OptionValue	Text COLLATE NOCASE
 );
 
-insert Into Options (OptionKey, OptionValue) values ('DBVersion', '20');
-insert Into Options (OptionKey, OptionValue) values ('Sequence', '1');
-insert Into Options (OptionKey, OptionValue) values ('EventSequence', '1');
-insert Into Options (OptionKey, OptionValue) values ('UpdateCount', '0');
 insert Into Options (OptionKey, OptionValue) values ('EvolutionLastModseq', '0');
 insert Into Options (OptionKey, OptionValue) values ('KMailLastModseq', '0');
-insert Into Options (OptionKey, OptionValue) values ('RssLastModseq', '0');
 insert Into Options (OptionKey, OptionValue) values ('CollationLocale', '');
-
 
