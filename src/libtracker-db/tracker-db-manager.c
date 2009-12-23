055ee877eb31c527f3e97b1d307e3e63f24f65a3
Martyn Russell <martyn@lanedo.com>
2009-12-23 14:09:12 +0000

    libtracker-db: New db version for coalesce/writeback schema changes

--- src/libtracker-db/tracker-db-manager.c
+++ src/libtracker-db/tracker-db-manager.c
@@ -50,7 +50,7 @@
 #define TRACKER_DB_PAGE_SIZE_DONT_SET -1
 
 /* Set current database version we are working with */
-#define TRACKER_DB_VERSION_NOW        TRACKER_DB_VERSION_7
+#define TRACKER_DB_VERSION_NOW        TRACKER_DB_VERSION_8
 #define TRACKER_DB_VERSION_FILE       "db-version.txt"
 
 typedef enum {
@@ -67,7 +67,8 @@ typedef enum {
 	TRACKER_DB_VERSION_4,       /* Version 0.6.92 (current TRUNK) */
 	TRACKER_DB_VERSION_5,       /* Version 0.7    (vstore branch) */
 	TRACKER_DB_VERSION_6,       /* Version 0.7.4  (nothing special) */
-	TRACKER_DB_VERSION_7        /* Version 0.7.12 (current master) */
+	TRACKER_DB_VERSION_7,       /* Version 0.7.12 (nmo ontology) */
+	TRACKER_DB_VERSION_8        /* Version 0.7.13 (coalesce & writeback) */
 } TrackerDBVersion;
 
 typedef struct {
