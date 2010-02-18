457bc47986917f9497d6d028cdbbff93a9cb4530
Carlos Garnacho <carlos@lanedo.com>
2010-02-18 18:29:01 +0100

    Use correct signature for TrackerCrawler::finished.

--- src/tracker-control/tracker-control.c
+++ src/tracker-control/tracker-control.c
@@ -187,13 +187,7 @@ crawler_check_file_cb (TrackerCrawler *crawler,
 
 static void
 crawler_finished_cb (TrackerCrawler *crawler,
-                     GQueue         *found,
-                     gboolean        was_interrupted,
-                     guint           directories_found,
-                     guint           directories_ignored,
-                     guint           files_found,
-                     guint           files_ignored,
-                     gpointer        user_data)
+                     gboolean        was_interrupted)
 {
 	g_main_loop_quit (user_data);
 }
