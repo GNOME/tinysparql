461666dcdbc70cde556163ab0d1d32e596539b4a
Carlos Garnacho <carlosg@gnome.org>
2010-04-16 14:06:34 +0200

    tracker-data-manager: Add tracker_data_manager_interrupt_thread()
    This is a thin layer on top of tracker_db_manager_interrupt_thread().

--- src/libtracker-data/tracker-data-manager.c
+++ src/libtracker-data/tracker-data-manager.c
@@ -2458,3 +2458,9 @@ tracker_data_manager_set_db_option_int64 (const gchar *option,
 	tracker_db_statement_execute (stmt, NULL);
 	g_object_unref (stmt);
 }
+
+gboolean
+tracker_data_manager_interrupt_thread (GThread *thread)
+{
+	return tracker_db_manager_interrupt_thread (thread);
+}
