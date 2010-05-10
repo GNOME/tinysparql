9f8bea0f80ee22cfb03f108c7cbab1d85e702ffd
Ivan Frade <ivan.frade@nokia.com>
2010-05-10 16:24:23 +0300

    Functional tests: duplicate code is evil

--- tests/functional-tests/force-sqlite-misused-batch.sh
+++ tests/functional-tests/force-sqlite-misused-batch.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
 
-if [[ ! -e ../../utils/data-generators/cc/generate ]]; then
-    echo "Run this from test/functional-tests/"
-    exit 2
-fi
-
-# If we don't have ttl files, bring them from the data-generators
-if [[ ! -d ttl ]]; then
-   CURRENT=`pwd`
-   cd ../../utils/data-generators/cc
-   # Generation takes time. Don't ask if they are already generated.
-   #  Checking just one random .ttl file... not good
-   if [[ ! -e ttl/032-nmo_Email.ttl ]]; then
-       ./generate max.cfg ;
-   else
-     echo "TTL directory already exist"
-   fi
-   cd $CURRENT
-   echo "Moving ttl generated files to functional-tests folder"
-   cp -R ../../utils/data-generators/cc/ttl . || (echo "Error generating ttl files" && exit 3)
-else
-   echo "TTL files already in place"
-fi
-
-echo "Cleaning tracker DBs and restarting"
-tracker-control -rs
+source helper-test-data.sh
 
 echo "Ready, now running the test"
 python2.5 11-sqlite-batch-misused.py
---
+++ tests/functional-tests/helper-test-data.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ ! -e ../../utils/data-generators/cc/generate ]]; then
+    echo "Run this from test/functional-tests/"
+    exit 2
+fi
+
+# If we don't have ttl files, bring them from the data-generators
+if [[ ! -d ttl ]]; then
+   CURRENT=`pwd`
+   cd ../../utils/data-generators/cc
+   # Generation takes time. Don't ask if they are already generated.
+   #  Checking just one random .ttl file... not good
+   if [[ ! -e ttl/032-nmo_Email.ttl ]]; then
+       ./generate max.cfg ;
+   else
+     echo "TTL directory already exist"
+   fi
+   cd $CURRENT
+   echo "Moving ttl generated files to functional-tests folder"
+   cp -R ../../utils/data-generators/cc/ttl . || (echo "Error generating ttl files" && exit 3)
+else
+   echo "TTL files already in place"
+fi
+
+echo "Cleaning tracker DBs and restarting"
+tracker-control -rs
