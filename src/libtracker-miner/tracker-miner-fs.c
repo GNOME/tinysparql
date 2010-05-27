GNOME/tinysparql

commit b724b6128124f53d8a4053674df7a19ffebe566b

do not escape display_name, it is already encoded in UTF-8
https://bugzilla.gnome.org/show_bug.cgi?id=619280

src/libtracker-miner/tracker-miner-fs.c
@@ -1651,7 +1651,7 @@ item_move (TrackerMinerFS *fs,
            GFile          *file,
            GFile          *source_file)
 {
-	gchar     *uri, *source_uri, *escaped_filename;
+	gchar     *uri, *source_uri;
 	GFileInfo *file_info;
 	GString   *sparql;
 	RecursiveMoveData move_data;
@@ -1745,8 +1745,6 @@ item_move (TrackerMinerFS *fs,
 	                        "} ",
 	                        source_iri, source_iri, source_iri);
 
-	escaped_filename = g_strescape (g_file_info_get_display_name (file_info), NULL);
-
 	g_string_append_printf (sparql,
 	                        "INSERT INTO <%s> {"
 	                        "  <%s> nfo:fileName \"%s\" ; "
@@ -1754,10 +1752,8 @@ item_move (TrackerMinerFS *fs,
 	                        "       nie:isStoredAs <%s> "
 	                        "} ",
 	                        source_iri, source_iri,
-	                        escaped_filename, uri,
+	                        g_file_info_get_display_name (file_info), uri,
 	                        source_iri);
-
-	g_free (escaped_filename);
 
 	move_data.main_loop = g_main_loop_new (NULL, FALSE);
 	move_data.sparql = sparql;

commit b88393608b911a471f9a065e2074c5dc75411e41

Fixed GB#619801 - We need SPARQL escaping for cases where we use g_file_info_get_display_name()
This was partially broken by commit b724b6128124f53d8a4053674df7a19ffebe566b

src/libtracker-miner/tracker-miner-fs.c
@@ -1657,6 +1657,7 @@ item_move (TrackerMinerFS *fs,
 	RecursiveMoveData move_data;
 	ProcessData *data;
 	gchar *source_iri;
+	gchar *display_name;
 	gboolean source_exists;
 
 	iri_cache_invalidate (fs, file);
@@ -1745,6 +1746,8 @@ item_move (TrackerMinerFS *fs,
 	                        "} ",
 	                        source_iri, source_iri, source_iri);
 
+	display_name = tracker_sparql_escape (g_file_info_get_display_name (file_info));
+
 	g_string_append_printf (sparql,
 	                        "INSERT INTO <%s> {"
 	                        "  <%s> nfo:fileName \"%s\" ; "
@@ -1752,8 +1755,10 @@ item_move (TrackerMinerFS *fs,
 	                        "       nie:isStoredAs <%s> "
 	                        "} ",
 	                        source_iri, source_iri,
-	                        g_file_info_get_display_name (file_info), uri,
+	                        display_name, uri,
 	                        source_iri);
+
+	g_free (display_name);
 
 	move_data.main_loop = g_main_loop_new (NULL, FALSE);
 	move_data.sparql = sparql;