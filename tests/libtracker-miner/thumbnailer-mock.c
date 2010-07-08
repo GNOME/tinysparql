f4891315438a1bf31be27fa067bf97946a3bf3ac
Aleksander Morgado <aleksander@lanedo.com>
2010-07-08 09:03:07 +0200

    libtracker-miner tests: proper file indentation

--- tests/libtracker-miner/thumbnailer-mock.c
+++ tests/libtracker-miner/thumbnailer-mock.c
@@ -28,23 +28,23 @@ static GList *calls = NULL;
 void
 dbus_mock_call_log_reset ()
 {
-        if (calls) {
-                g_list_foreach (calls, (GFunc)g_free, NULL);
-                g_list_free (calls);
-                calls = NULL;
-        }
+	if (calls) {
+		g_list_foreach (calls, (GFunc)g_free, NULL);
+		g_list_free (calls);
+		calls = NULL;
+	}
 }
 
 GList *
 dbus_mock_call_log_get ()
 {
-        return calls;
+	return calls;
 }
 
 static void
 dbus_mock_call_log_append (const gchar *function_name)
 {
-        calls = g_list_append (calls, g_strdup (function_name));
+	calls = g_list_append (calls, g_strdup (function_name));
 }
 
 
@@ -56,7 +56,7 @@ dbus_mock_call_log_append (const gchar *function_name)
 DBusGConnection *
 dbus_g_bus_get (DBusBusType type, GError **error)
 {
-        return (DBusGConnection *) empty_object_new ();
+	return (DBusGConnection *) empty_object_new ();
 }
 
 DBusGProxy *
@@ -65,7 +65,7 @@ dbus_g_proxy_new_for_name (DBusGConnection *connection,
                            const gchar *path,
                            const gchar *interface )
 {
-        return (DBusGProxy *) empty_object_new ();
+	return (DBusGProxy *) empty_object_new ();
 }
 
 gboolean
@@ -74,48 +74,48 @@ dbus_g_proxy_call (DBusGProxy *proxy,
                    GError  **error,
                    GType first_arg_type, ...)
 {
-        va_list args;
-        GType arg_type;
-        const gchar *supported_mimes[] = { "mock/one", "mock/two", NULL};
-        int     counter;
+	va_list args;
+	GType arg_type;
+	const gchar *supported_mimes[] = { "mock/one", "mock/two", NULL};
+	int     counter;
 
-        g_assert (g_strcmp0 (function_name, "GetSupported") == 0);
+	g_assert (g_strcmp0 (function_name, "GetSupported") == 0);
 
-        /*
-	   G_TYPE_INVALID,
-	   G_TYPE_STRV, &uri_schemes,
-	   G_TYPE_STRV, &mime_types,
-	   G_TYPE_INVALID);
+	/*
+	  G_TYPE_INVALID,
+	  G_TYPE_STRV, &uri_schemes,
+	  G_TYPE_STRV, &mime_types,
+	  G_TYPE_INVALID);
 
-           Set the mock values in the second parameter :)
-        */
+	  Set the mock values in the second parameter :)
+	*/
 
-        va_start (args, first_arg_type);
-        arg_type = va_arg (args, GType);
+	va_start (args, first_arg_type);
+	arg_type = va_arg (args, GType);
 
-        counter = 1;
-        while (arg_type != G_TYPE_INVALID) {
+	counter = 1;
+	while (arg_type != G_TYPE_INVALID) {
 
-                if (arg_type == G_TYPE_STRV && counter == 2) {
-	                gchar *local_error = NULL;
-                        GValue value = { 0, };
-                        g_value_init (&value, arg_type);
-                        g_value_set_boxed (&value, supported_mimes);
-                        G_VALUE_LCOPY (&value,
-                                       args, 0,
-                                       &local_error);
-                        g_free (local_error);
-                        g_value_unset (&value);
-                } else {
-                        gpointer *out_param;
-                        out_param = va_arg (args, gpointer *);
-                }
-                arg_type = va_arg (args, GType);
-                counter += 1;
-        }
+		if (arg_type == G_TYPE_STRV && counter == 2) {
+			gchar *local_error = NULL;
+			GValue value = { 0, };
+			g_value_init (&value, arg_type);
+			g_value_set_boxed (&value, supported_mimes);
+			G_VALUE_LCOPY (&value,
+			               args, 0,
+			               &local_error);
+			g_free (local_error);
+			g_value_unset (&value);
+		} else {
+			gpointer *out_param;
+			out_param = va_arg (args, gpointer *);
+		}
+		arg_type = va_arg (args, GType);
+		counter += 1;
+	}
 
-        va_end (args);
-        return TRUE;
+	va_end (args);
+	return TRUE;
 }
 
 
@@ -125,7 +125,7 @@ dbus_g_proxy_call_no_reply (DBusGProxy        *proxy,
                             GType              first_arg_type,
                             ...)
 {
-        dbus_mock_call_log_append (method);
+	dbus_mock_call_log_append (method);
 }
 
 
