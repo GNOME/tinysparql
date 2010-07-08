7d9b57fc3e971bd60801edde08e5ca0e4ad3ef17
Aleksander Morgado <aleksander@lanedo.com>
2010-07-08 11:55:00 +0200

    libtracker-miner, storage: improve logging

--- src/libtracker-miner/tracker-storage.c
+++ src/libtracker-miner/tracker-storage.c
@@ -620,8 +620,12 @@ mount_add (TrackerStorage *storage,
 									      -1);
 					g_debug ("  No UUID, generated:'%s' (based on mount path)", uuid);
 				} else {
-					g_debug ("  Being ignored because mount is music/video/blank, content type is '%s'",
-						 content_type);
+					g_debug ("  Being ignored because mount is music/video/blank "
+					         "(content type:%s, optical:%s, multimedia:%s, blank:%s)",
+					         content_type,
+					         is_optical ? "yes" : "no",
+					         is_multimedia ? "yes" : "no",
+					         is_blank ? "yes" : "no");
 				}
 
 				g_free (content_type);
