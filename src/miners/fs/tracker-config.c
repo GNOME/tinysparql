ea3e1045bea57b2fa8c96e8303788e8cf421b48f
Carlos Garnacho <carlosg@gnome.org>
2010-08-31 13:01:50 +0200

    Remove CacheTimeout option from tracker-miner-fs config.
    This option has been unused for quite some time now.

--- src/miners/fs/tracker-config.c
+++ src/miners/fs/tracker-config.c
@@ -44,7 +44,6 @@
 #define DEFAULT_INITIAL_SLEEP                    15       /* 0->1000 */
 #define DEFAULT_ENABLE_MONITORS                  TRUE
 #define DEFAULT_THROTTLE                         0        /* 0->20 */
-#define DEFAULT_CACHE_TIMEOUT                    60       /* 0->1000 */
 #define DEFAULT_INDEX_REMOVABLE_DEVICES          TRUE
 #define DEFAULT_INDEX_OPTICAL_DISCS              FALSE
 #define DEFAULT_INDEX_ON_BATTERY                 FALSE
@@ -59,7 +58,6 @@ typedef struct {
 
 	/* Monitors */
 	gboolean  enable_monitors;
-	gint      cache_timeout;
 
 	/* Indexing */
 	gint      throttle;
@@ -118,7 +116,6 @@ enum {
 
 	/* Monitors */
 	PROP_ENABLE_MONITORS,
-	PROP_CACHE_TIMEOUT,
 
 	/* Indexing */
 	PROP_THROTTLE,
@@ -201,15 +198,6 @@ tracker_config_class_init (TrackerConfigClass *klass)
 	                                                       " Set to false to completely disable any monitoring",
 	                                                       DEFAULT_ENABLE_MONITORS,
 	                                                       G_PARAM_READWRITE | G_PARAM_CONSTRUCT));
-	g_object_class_install_property (object_class,
-	                                 PROP_CACHE_TIMEOUT,
-	                                 g_param_spec_int ("cache-timeout",
-	                                                   "Scan Timeout",
-	                                                   " Time in seconds for events to be cached (0->1000)",
-	                                                   0,
-	                                                   1000,
-	                                                   DEFAULT_CACHE_TIMEOUT,
-	                                                   G_PARAM_READWRITE | G_PARAM_CONSTRUCT));
 
 	/* Indexing */
 	g_object_class_install_property (object_class,
@@ -344,10 +332,6 @@ config_set_property (GObject      *object,
 		tracker_config_set_enable_monitors (TRACKER_CONFIG (object),
 		                                    g_value_get_boolean (value));
 		break;
-	case PROP_CACHE_TIMEOUT:
-		tracker_config_set_cache_timeout (TRACKER_CONFIG (object),
-		                                  g_value_get_int (value));
-		break;
 
 		/* Indexing */
 	case PROP_THROTTLE:
@@ -427,9 +411,6 @@ config_get_property (GObject    *object,
 		/* Montors */
 	case PROP_ENABLE_MONITORS:
 		g_value_set_boolean (value, priv->enable_monitors);
-		break;
-	case PROP_CACHE_TIMEOUT:
-		g_value_set_int (value, priv->cache_timeout);
 		break;
 
 		/* Indexing */
@@ -1091,18 +1072,6 @@ tracker_config_get_enable_monitors (TrackerConfig *config)
 }
 
 gint
-tracker_config_get_cache_timeout (TrackerConfig *config)
-{
-	TrackerConfigPrivate *priv;
-
-	g_return_val_if_fail (TRACKER_IS_CONFIG (config), DEFAULT_CACHE_TIMEOUT);
-
-	priv = TRACKER_CONFIG_GET_PRIVATE (config);
-
-	return priv->cache_timeout;
-}
-
-gint
 tracker_config_get_throttle (TrackerConfig *config)
 {
 	TrackerConfigPrivate *priv;
@@ -1318,24 +1287,6 @@ tracker_config_set_enable_monitors (TrackerConfig *config,
 
 	priv->enable_monitors = value;
 	g_object_notify (G_OBJECT (config), "enable-monitors");
-}
-
-void
-tracker_config_set_cache_timeout (TrackerConfig *config,
-                                  gint           value)
-{
-	TrackerConfigPrivate *priv;
-
-	g_return_if_fail (TRACKER_IS_CONFIG (config));
-
-	if (!tracker_keyfile_object_validate_int (config, "cache-timeout", value)) {
-		return;
-	}
-
-	priv = TRACKER_CONFIG_GET_PRIVATE (config);
-
-	priv->cache_timeout = value;
-	g_object_notify (G_OBJECT (config), "cache-timeout");
 }
 
 void
