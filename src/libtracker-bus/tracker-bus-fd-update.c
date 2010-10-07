a74e45ba583a4bd10e43841ada853ccfc98f7271
Philip Van Hoof <philip@codeminded.be>
2010-10-07 12:40:39 +0200

    libtracker-bus: multi-insert: Fix memory leak

--- src/libtracker-bus/tracker-bus-fd-update.c
+++ src/libtracker-bus/tracker-bus-fd-update.c
@@ -267,6 +267,7 @@ sparql_update_array_fast_callback (DBusPendingCall *call,
 
 		g_simple_async_result_set_op_res_gpointer (fad->res, errors, NULL);
 		g_simple_async_result_complete (fad->res);
+		g_ptr_array_free (errors, TRUE);
 		break;
 	default:
 		g_assert_not_reached ();
