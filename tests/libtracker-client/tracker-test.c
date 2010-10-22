9855fc862e16f866eaa565c24362a7330cd31915
Martyn Russell <martyn@lanedo.com>
2010-10-22 18:03:50 +0100

    build: Remove WARN_CFLAGS and GCOV_{CFLAGS|LIBS}, use BUILD_{CFLAGS|LIBS} instead

--- tests/libtracker-client/tracker-test.c
+++ tests/libtracker-client/tracker-test.c
@@ -16,8 +16,10 @@
  * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA  02110-1301, USA.
  */
+
 #include <glib.h>
-#include <libtracker-client/tracker.h>
+
+#include <libtracker-client/tracker-client.h>
 
 static void
 test_tracker_client ()
