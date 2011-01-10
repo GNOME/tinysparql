c8c751a086095ac1a2d565514ef85b714fbff973
Philip Van Hoof <philip@codeminded.be>
2011-01-10 11:37:37 +0100

    miner-rss: GDBus port of tracker-miner-rss.c

--- src/libtracker-common/tracker-ontologies.h
+++ src/libtracker-common/tracker-ontologies.h
@@ -47,6 +47,7 @@ G_BEGIN_DECLS
 /* Temporary */
 #define TRACKER_NMM_PREFIX      "http://www.tracker-project.org/temp/nmm#"
 #define TRACKER_MLO_PREFIX      "http://www.tracker-project.org/temp/mlo#"
+#define TRACKER_MFO_PREFIX      "http://www.tracker-project.org/temp/mfo#"
 
 #define TRACKER_DATASOURCE_URN_PREFIX	  \
 	"urn:nepomuk:datasource:"
