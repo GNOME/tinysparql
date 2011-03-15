381ab65d6b3ab6128b0f8ab72040f8070a70e82a
Aleksander Morgado <aleksander@lanedo.com>
2011-03-15 14:41:16 +0100

    tracker-miner-fs: Set limit of requests in processing pool to 10

--- src/miners/fs/tracker-miner-files.c
+++ src/miners/fs/tracker-miner-files.c
@@ -2708,6 +2708,7 @@ tracker_miner_files_new (TrackerConfig  *config,
 	                       "config", config,
 	                       "processing-pool-wait-limit", 10,
 	                       "processing-pool-ready-limit", 100,
+	                       "processing-pool-requests-limit", 10,
 	                       NULL);
 }
 
