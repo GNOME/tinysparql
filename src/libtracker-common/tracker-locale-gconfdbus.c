76952de12fe865fa26f3b44d28df39e79816aaae
Philip Van Hoof <philip@codeminded.be>
2011-04-05 11:16:31 +0200

    libtracker-common: Use 2011 for copyright of new files

--- src/libtracker-common/tracker-locale-gconfdbus.c
+++ src/libtracker-common/tracker-locale-gconfdbus.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 Nokia <ivan.frade@nokia.com>
+ * Copyright (C) 2011 Nokia <ivan.frade@nokia.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
