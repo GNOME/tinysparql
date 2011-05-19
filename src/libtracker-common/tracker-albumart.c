5422ee33eb987b190b03e8ce72b93ccaedc85f89
Martyn Russell <martyn@lanedo.com>
2011-05-19 16:06:08 +0100

    libtracker-common: Fixed documentation version typo, s/0.10.4/0.10.14/

--- src/libtracker-common/tracker-albumart.c
+++ src/libtracker-common/tracker-albumart.c
@@ -84,7 +84,7 @@ albumart_strip_find_next_block (const gchar    *original,
  *
  * Returns: copy of original but then stripped
  *
- * Since: 0.10.4
+ * Since: 0.10.14
  */
 gchar *
 tracker_albumart_strip_invalid_entities (const gchar *original)
@@ -212,7 +212,7 @@ albumart_checksum_for_data (GChecksumType  checksum_type,
  * Get the path to albumart for a given album or song. Newly allocated data in
  * @path and @local_uri must be freed with g_free.
  *
- * Since: 0.10.4
+ * Since: 0.10.14
  */
 void
 tracker_albumart_get_path (const gchar  *artist,
