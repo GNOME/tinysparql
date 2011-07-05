f71b1ac1adec7c66d67d4eaa8da9d0e2e29131b4
Carlos Garnacho <carlos@lanedo.com>
2011-07-05 18:20:55 +0200

    libtracker-miner: Use TrackerSparqlBuffer for metadata insertions
    As a side effect, the TrackerMinerFS::processing-pool-requests-limit property
    has been removed, as flow control has been simplified to not needing this third
    queue.

--- src/miners/fs/tracker-miner-files.c
+++ src/miners/fs/tracker-miner-files.c
@@ -2537,7 +2537,6 @@ tracker_miner_files_new (TrackerConfig  *config,
 	                       "config", config,
 	                       "processing-pool-wait-limit", 10,
 	                       "processing-pool-ready-limit", 100,
-	                       "processing-pool-requests-limit", 10,
 	                       NULL);
 }
 
