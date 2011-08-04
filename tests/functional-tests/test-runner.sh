f7e82e33e0876c31471769ff9442a1c6f61884fb
Sam Thursfield <samthursfield@codethink.co.uk>
2011-08-04 17:43:40 +0100

    functional-tests: Run tests inside an isolated DBus instance

--- tests/functional-tests/test-runner.sh
+++ tests/functional-tests/test-runner.sh
@@ -2,11 +2,19 @@
 #
 # Test runner script for Tracker's functional tests
 
+DBUS_SESSION_BUS_PID=
+
 if test -h /targets/links/scratchbox.config ; then
     export SBOX_REDIRECT_IGNORE=/usr/bin/python ;
 
     meego-run $@
 else
+    eval `dbus-launch --sh-syntax`
+
+    trap "/bin/kill $DBUS_SESSION_BUS_PID; exit" INT
+
     echo "Running $@"
     $@
+
+    kill $DBUS_SESSION_BUS_PID
 fi ;
