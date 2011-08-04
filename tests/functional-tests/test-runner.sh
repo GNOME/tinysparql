bcc657584140c05e0bc3a11cceb183f4893bab09
Sam Thursfield <samthursfield@codethink.co.uk>
2011-08-04 17:42:54 +0100

    functional-tests: Separate out test-runner.sh from Makefile

---
+++ tests/functional-tests/test-runner.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Test runner script for Tracker's functional tests
+
+if test -h /targets/links/scratchbox.config ; then
+    export SBOX_REDIRECT_IGNORE=/usr/bin/python ;
+
+    meego-run $@
+else
+    $@
+fi ;
