515e2f928979d57356dd1a2e6b87211b7c1e8c73
Aleksander Morgado <aleksander@lanedo.com>
2011-12-15 16:54:29 +0100

    libtracker-miner: fix deprecation comment format

--- src/libtracker-miner/tracker-miner-fs.c
+++ src/libtracker-miner/tracker-miner-fs.c
@@ -452,7 +452,7 @@ tracker_miner_fs_class_init (TrackerMinerFSClass *klass)
 	 *
 	 * Since: 0.8
 	 *
-	 * Deprecated since: 0.12
+	 * Deprecated: 0.12
 	 **/
 	signals[IGNORE_NEXT_UPDATE_FILE] =
 		g_signal_new ("ignore-next-update-file",
