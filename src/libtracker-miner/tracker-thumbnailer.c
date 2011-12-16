d9c26a8270b905332fb947ccc74946cab3f75923
Aleksander Morgado <aleksander@lanedo.com>
2011-12-16 12:44:45 +0100

    libtracker-miner: use GPrivate if GLib >= 2.31
    Since 2.31, GStaticPrivate is deprecated.

--- src/libtracker-miner/tracker-thumbnailer.c
+++ src/libtracker-miner/tracker-thumbnailer.c
@@ -62,7 +62,12 @@ typedef struct {
 	gboolean service_is_available;
 } TrackerThumbnailerPrivate;
 
+#if GLIB_CHECK_VERSION (2,31,0)
+static void private_free (gpointer data);
+static GPrivate private_key = G_PRIVATE_INIT (private_free);
+#else
 static GStaticPrivate private_key = G_STATIC_PRIVATE_INIT;
+#endif
 
 static void
 private_free (gpointer data)
@@ -140,9 +145,11 @@ tracker_thumbnailer_init (void)
 	/* Don't start at 0, start at 1. */
 	private->request_id = 1;
 
-	g_static_private_set (&private_key,
-	                      private,
-	                      private_free);
+#if GLIB_CHECK_VERSION (2,31,0)
+	g_private_replace (&private_key, private);
+#else
+	g_static_private_set (&private_key, private, private_free);
+#endif
 
 	g_message ("Thumbnailer connections being set up...");
 
@@ -268,7 +275,11 @@ error_handler:
 void
 tracker_thumbnailer_shutdown (void)
 {
+#if GLIB_CHECK_VERSION (2,31,0)
+	g_private_replace (&private_key, NULL);
+#else
 	g_static_private_set (&private_key, NULL, NULL);
+#endif
 }
 
 /**
@@ -297,7 +308,11 @@ tracker_thumbnailer_move_add (const gchar *from_uri,
 	g_return_val_if_fail (from_uri != NULL, FALSE);
 	g_return_val_if_fail (to_uri != NULL, FALSE);
 
+#if GLIB_CHECK_VERSION (2,31,0)
+	private = g_private_get (&private_key);
+#else
 	private = g_static_private_get (&private_key);
+#endif
 	g_return_val_if_fail (private != NULL, FALSE);
 
 	if (!private->service_is_available) {
@@ -340,7 +355,11 @@ tracker_thumbnailer_remove_add (const gchar *uri,
 
 	g_return_val_if_fail (uri != NULL, FALSE);
 
+#if GLIB_CHECK_VERSION (2,31,0)
+	private = g_private_get (&private_key);
+#else
 	private = g_static_private_get (&private_key);
+#endif
 	g_return_val_if_fail (private != NULL, FALSE);
 
 	if (!private->service_is_available) {
@@ -375,7 +394,11 @@ tracker_thumbnailer_cleanup (const gchar *uri_prefix)
 
 	g_return_val_if_fail (uri_prefix != NULL, FALSE);
 
+#if GLIB_CHECK_VERSION (2,31,0)
+	private = g_private_get (&private_key);
+#else
 	private = g_static_private_get (&private_key);
+#endif
 	g_return_val_if_fail (private != NULL, FALSE);
 
 	if (!private->service_is_available) {
@@ -413,7 +436,11 @@ tracker_thumbnailer_send (void)
 	TrackerThumbnailerPrivate *private;
 	guint list_len;
 
+#if GLIB_CHECK_VERSION (2,31,0)
+	private = g_private_get (&private_key);
+#else
 	private = g_static_private_get (&private_key);
+#endif
 	g_return_if_fail (private != NULL);
 
 	if (!private->service_is_available) {
