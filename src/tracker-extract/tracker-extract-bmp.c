3c1eb6dde4633ad0bf0d8919d0b0ca7acde0de94
Philip Van Hoof <philip@codeminded.be>
2014-03-02 14:02:16 +0100

    Add an empty BMP extractor to get the rdf:type for the file type right

---
+++ src/tracker-extract/tracker-extract-bmp.c
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2013-2014 Jolla Ltd. <andrew.den.exter@jollamobile.com>
+ * Author: Philip Van Hoof <philip@codeminded.be>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+
+#include "config.h"
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include <libtracker-common/tracker-common.h>
+#include <libtracker-extract/tracker-extract.h>
+
+
+G_MODULE_EXPORT gboolean
+tracker_extract_get_metadata (TrackerExtractInfo *info)
+{
+	TrackerSparqlBuilder *preupdate, *metadata;
+	goffset size;
+	const gchar *graph;
+	gchar *filename, *uri;
+	GFile *file;
+
+	preupdate = tracker_extract_info_get_preupdate_builder (info);
+	metadata = tracker_extract_info_get_metadata_builder (info);
+	graph = tracker_extract_info_get_graph (info);
+
+	file = tracker_extract_info_get_file (info);
+	filename = g_file_get_path (file);
+	size = tracker_file_get_size (filename);
+
+	if (size < 14) {
+		/* Smaller than BMP header, can't be a real BMP file */
+		g_free (filename);
+		return FALSE;
+	}
+
+	tracker_sparql_builder_predicate (metadata, "a");
+	tracker_sparql_builder_object (metadata, "nfo:Image");
+	tracker_sparql_builder_object (metadata, "nmm:Photo");
+
+	/* TODO: Add actual metadata extraction for BMP files */
+
+	return TRUE;
+}
