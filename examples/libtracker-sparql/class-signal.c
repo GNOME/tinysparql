963e53eb79a5b727ef008a875f16005e4c6d67dd
Martyn Russell <martyn@lanedo.com>
2014-03-20 11:15:17 +0000

    libtracker-sparql: Don't use internal tracker_ipc_bus() function in examples/
    We don't want people copying this code and then having linking problems and
    3rd party apps using libtracker-sparql shouldn't be depending on internal
    functions or libraries like libtracker-common.

--- examples/libtracker-sparql/class-signal.c
+++ examples/libtracker-sparql/class-signal.c
@@ -19,8 +19,6 @@
 
 #include <gio/gio.h>
 
-#include <libtracker-common/tracker-common.h>
-
 #include <libtracker-sparql/tracker-sparql.h>
 
 static TrackerSparqlConnection *con;
@@ -89,7 +87,7 @@ main (gint argc, gchar *argv[])
 
 	loop = g_main_loop_new (NULL, FALSE);
 	con = tracker_sparql_connection_get (NULL, &error);
-	connection = g_bus_get_sync (TRACKER_IPC_BUS, NULL, NULL);
+	connection = g_bus_get_sync (G_BUS_TYPE_SESSION, NULL, NULL);
 
 	signal_id = g_dbus_connection_signal_subscribe (connection,
 	                                                TRACKER_DBUS_SERVICE,
