9dfde8d4587e74a0291084faa1cce9e3bc441552
Martyn Russell <martyn@lanedo.com>
2014-03-21 11:50:05 +0000

    libtracker-miner, tracker-extract: Don't g_warning() on decorator items we don't process
    In normal circumstances, we may get TRACKER_DECORATOR_ERROR_{EMPTY|PAUSED},
    but tracker-extract issues a g_warning(). We want to avoid using a warning for
    these warnings because they're normal and expected operations.

--- src/libtracker-miner/tracker-decorator.c
+++ src/libtracker-miner/tracker-decorator.c
@@ -94,15 +94,9 @@ enum {
 	LAST_SIGNAL
 };
 
-typedef enum {
-	TRACKER_DECORATOR_ERROR_EMPTY,
-	TRACKER_DECORATOR_ERROR_PAUSED
-} TrackerDecoratorError;
-
 static guint signals[LAST_SIGNAL] = { 0 };
 static GInitableIface *parent_initable_iface;
 
-static GQuark tracker_decorator_error_quark         (void);
 static void   tracker_decorator_initable_iface_init (GInitableIface   *iface);
 
 G_DEFINE_QUARK (TrackerDecoratorError, tracker_decorator_error)
