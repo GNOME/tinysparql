e0a808530b1eb7afaf7a02b745e1a889680309c5
Giovanni Campagna <gcampagna@src.gnome.org>
2014-07-17 15:21:59 +0200

    tracker-extract: recognize application/vnd.ms-asf for the gstreamer plugin
    application/vnd.ms-asf is the new standard name for video/x-ms-asf.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=733317

--- src/tracker-extract/tracker-extract-gstreamer.c
+++ src/tracker-extract/tracker-extract-gstreamer.c
@@ -1699,7 +1699,8 @@ tracker_extract_get_metadata (TrackerExtractInfo *info)
 		tracker_extract_gstreamer (uri, info, EXTRACT_MIME_SVG, graph);
 	} else if (strcmp (mimetype, "video/3gpp") == 0 ||
 	           strcmp (mimetype, "video/mp4") == 0 ||
-	           strcmp (mimetype, "video/x-ms-asf") == 0 ||
+                   strcmp (mimetype, "video/x-ms-asf") == 0 ||
+                   strcmp (mimetype, "application/vnd.ms-asf") == 0 ||
 	           strcmp (mimetype, "application/vnd.rn-realmedia") == 0) {
 		tracker_extract_gstreamer (uri, info, EXTRACT_MIME_GUESS, graph);
 	} else if (g_str_has_prefix (mimetype, "audio/")) {
