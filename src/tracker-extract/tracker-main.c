a9b45db52edf52175e4fe4f39bf24b7635c0f265
Sam Thursfield <sam@afuera.me.uk>
2014-07-31 13:55:08 +0100

    tracker-extract: Use default signal handler for SIGALRM and SIGABRT
    The ALRM handler was introduced in commit
    d9d1881c2548e5d6d55fad1e897f8f058ef28696. The alarm() function seems to
    no longer be used, so we can remove it.
    
    The ABRT handler was introduced in commit
    3f42a4390d48c6a4840a7bcfce74673ebfd23479. I'm not sure of the reason.
    Other Tracker processes use the default handler for ABRT, so let's be
    consistent.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=734144

--- src/tracker-extract/tracker-main.c
+++ src/tracker-extract/tracker-main.c
@@ -166,10 +166,6 @@ signal_handler (int signo)
 	}
 
 	switch (signo) {
-	case SIGABRT:
-	case SIGALRM:
-		_exit (EXIT_FAILURE);
-		break;
 	case SIGTERM:
 	case SIGINT:
 		in_loop = TRUE;
@@ -202,8 +198,6 @@ initialize_signal_handler (void)
 	sigaction (SIGTERM, &act, NULL);
 	sigaction (SIGINT,  &act, NULL);
 	sigaction (SIGHUP,  &act, NULL);
-	sigaction (SIGALRM,  &act, NULL);
-	sigaction (SIGABRT, &act, NULL);
 #endif /* G_OS_WIN32 */
 }
 
