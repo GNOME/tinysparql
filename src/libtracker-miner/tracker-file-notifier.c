4835e9e06cf1320f74b7975724753ae1afd17711
Ting-Wei Lan <lantw@src.gnome.org>
2015-03-18 15:12:55 +0800

    Fix "void function should not return a value"
    https://bugzilla.gnome.org/show_bug.cgi?id=746381

--- src/libtracker-miner/tracker-file-notifier.c
+++ src/libtracker-miner/tracker-file-notifier.c
@@ -1732,8 +1732,8 @@ tracker_file_notifier_invalidate_file_iri (TrackerFileNotifier *notifier,
 	TrackerFileNotifierPrivate *priv;
 	GFile *canonical;
 
-	g_return_val_if_fail (TRACKER_IS_FILE_NOTIFIER (notifier), NULL);
-	g_return_val_if_fail (G_IS_FILE (file), NULL);
+	g_return_if_fail (TRACKER_IS_FILE_NOTIFIER (notifier));
+	g_return_if_fail (G_IS_FILE (file));
 
 	priv = notifier->priv;
 	canonical = tracker_file_system_get_file (priv->file_system,
