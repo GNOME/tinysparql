143665b3fda4b0e1acaaa9033bcca7b28177d748
Carlos Garnacho <carlosg@gnome.org>
2016-05-25 00:55:53 +0200

    tracker-miner-fs: Replace direct emission of TrackerIndexingTree signals
    We now have tracker_indexing_tree_notify_update().

--- src/miners/fs/tracker-miner-files.c
+++ src/miners/fs/tracker-miner-files.c
@@ -1739,7 +1739,7 @@ miner_files_force_recheck_idle (gpointer user_data)
 	for (l = roots; l; l = l->next)	{
 		GFile *root = l->data;
 
-		g_signal_emit_by_name (indexing_tree, "directory-updated", root);
+		tracker_indexing_tree_notify_update (indexing_tree, root, FALSE);
 	}
 
 	miner_files->private->force_recheck_id = 0;
