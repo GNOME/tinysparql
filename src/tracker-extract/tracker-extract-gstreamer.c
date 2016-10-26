5e9f0b5437ca9a42705be9e0e354bba5e5c0eb26
Marinus Schraal <mschraal@src.gnome.org>
2016-10-26 12:06:36 +0200

    tracker-extract: Only use CUE info as last resort in gstreamer
    CUE data was overriding file tags in the gstreamer extractor, however
    CUE data is notoriously unreliable. Instead, only use the CUE data if we
    have nothing else.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=773524

--- src/tracker-extract/tracker-extract-gstreamer.c
+++ src/tracker-extract/tracker-extract-gstreamer.c
@@ -892,7 +892,7 @@ extract_metadata (MetadataExtractor      *extractor,
 	if (extractor->toc) {
 		gst_tag_list_insert (extractor->tagcache,
 		                     extractor->toc->tag_list,
-		                     GST_TAG_MERGE_REPLACE);
+		                     GST_TAG_MERGE_KEEP);
 
 		if (g_list_length (extractor->toc->entry_list) == 1) {
 			/* If we only got one track, stick all the info together and
@@ -903,7 +903,7 @@ extract_metadata (MetadataExtractor      *extractor,
 			toc_entry = extractor->toc->entry_list->data;
 			gst_tag_list_insert (extractor->tagcache,
 			                     toc_entry->tag_list,
-			                     GST_TAG_MERGE_REPLACE);
+			                     GST_TAG_MERGE_KEEP);
 
 			tracker_toc_free (extractor->toc);
 			extractor->toc = NULL;
