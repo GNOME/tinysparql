9c09fdc9f3e13ae589d92c30838bbf066eac1ff9
Carlos Garnacho <carlosg@gnome.org>
2016-10-26 19:50:42 +0200

    *: Drop code not meant for this repo
    This repo now contains tracker-miner-rss solely.

--- src/plugins/firefox/defaults/preferences/prefs.js
+++
@@ -1,2 +0,0 @@
-// https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.trackerfox@bustany.org.description", "chrome://trackerfox/locale/overlay.properties");
--- tests/functional-tests/unittest2/unit2
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-__unittest = True
-
-from unittest2.main import main, TestProgram, USAGE_AS_MAIN
-TestProgram.USAGE = USAGE_AS_MAIN
-
-main(module=None)
