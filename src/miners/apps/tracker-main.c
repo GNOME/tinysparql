3e51f55538eaf3e1882131ce46214560504a3c55
Carlos Garnacho <carlosg@gnome.org>
2017-06-21 21:44:15 +0200

    tracker-miner-apps: If there is a domain, set watch on it
    And exit as soon as the name vanishes. It may safely continue
    its operations on the next startup.

--- src/miners/apps/tracker-main.c
+++ src/miners/apps/tracker-main.c
@@ -167,6 +167,15 @@ miner_finished_cb (TrackerMinerFS *fs,
 	}
 }
 
+static void
+on_domain_vanished (GDBusConnection *connection,
+                    const gchar     *name,
+                    gpointer         user_data)
+{
+	GMainLoop *loop = user_data;
+	g_main_loop_quit (loop);
+}
+
 int
 main (gint argc, gchar *argv[])
 {
@@ -250,6 +259,13 @@ main (gint argc, gchar *argv[])
 	initialize_priority_and_scheduling (TRACKER_SCHED_IDLE_ALWAYS, FALSE);
 
 	main_loop = g_main_loop_new (NULL, FALSE);
+
+	if (domain_ontology && domain_ontology_name) {
+		g_bus_watch_name_on_connection (connection, domain_ontology_name,
+		                                G_BUS_NAME_WATCHER_FLAGS_NONE,
+		                                NULL, on_domain_vanished,
+		                                main_loop, NULL);
+	}
 
 	g_message ("Checking if we're running as a daemon:");
 	g_message ("  %s %s",
