68404259663f44ec3892f61d3e5f5c538eb66482
Carlos Garnacho <carlosg@gnome.org>
2017-06-21 21:11:42 +0200

    tracker-miner-rss: If there is a domain, set watch on it
    And exit as soon as the name vanishes. It may safely continue
    its operations on the next startup.

--- src/miners/rss/tracker-main.c
+++ src/miners/rss/tracker-main.c
@@ -59,6 +59,15 @@ static GOptionEntry entries[] = {
 	{ NULL }
 };
 
+static void
+on_domain_vanished (GDBusConnection *connection,
+                    const gchar     *name,
+                    gpointer         user_data)
+{
+	GMainLoop *loop = user_data;
+	g_main_loop_quit (loop);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -207,6 +216,14 @@ main (int argc, char **argv)
 	}
 
 	loop = g_main_loop_new (NULL, FALSE);
+
+	if (domain_ontology && domain_ontology_name) {
+		g_bus_watch_name_on_connection (connection, domain_ontology_name,
+		                                G_BUS_NAME_WATCHER_FLAGS_NONE,
+		                                NULL, on_domain_vanished,
+		                                loop, NULL);
+	}
+
 	g_main_loop_run (loop);
 
 	tracker_log_shutdown ();
