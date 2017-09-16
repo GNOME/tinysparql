4c95a0113002bbddaee1a4d481bd39b29e4f6c4a
Debarshi Ray <debarshir@gnome.org>
2017-09-17 01:35:02 +0200

    tracker-extract: Fix insertion of white balance values
    https://bugzilla.gnome.org/show_bug.cgi?id=787774

--- src/tracker-extract/tracker-extract-pdf.c
+++ src/tracker-extract/tracker-extract-pdf.c
@@ -481,7 +481,7 @@ tracker_extract_get_metadata (TrackerExtractInfo *info)
 			TrackerResource *white_balance;
 
 			white_balance = tracker_resource_new (xd->white_balance);
-			tracker_resource_set_relation (metadata, "nmm:meteringMode", white_balance);
+			tracker_resource_set_relation (metadata, "nmm:whiteBalance", white_balance);
 			g_object_unref (white_balance);
 		}
 
--- src/tracker-extract/tracker-extract-png.c
+++ src/tracker-extract/tracker-extract-png.c
@@ -433,7 +433,7 @@ read_metadata (TrackerResource      *metadata,
 		TrackerResource *white_balance;
 
 		white_balance = tracker_resource_new (md.white_balance);
-		tracker_resource_set_relation (metadata, "nmm:meteringMode", white_balance);
+		tracker_resource_set_relation (metadata, "nmm:whiteBalance", white_balance);
 		g_object_unref (white_balance);
 	}
 
