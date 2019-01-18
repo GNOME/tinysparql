3a164cca68226e3f0eea6f9a1d0ae3900b8c1e2a
Andrea Azzarone <andrea.azzarone@canonical.com>
2019-01-18 15:52:07 +0000

    tests: Use tmp dir on tests

--- tests/libtracker-data/tracker-backup-test.c
+++ tests/libtracker-data/tracker-backup-test.c
@@ -267,8 +267,10 @@ main (int argc, char **argv)
 	setlocale (LC_COLLATE, "en_US.utf8");
 
 	current_dir = g_get_current_dir ();
-	tests_data_dir = g_build_path (G_DIR_SEPARATOR_S, current_dir, "backup-test-data", NULL);
+	tests_data_dir = g_build_filename (current_dir, "backup-test-data-XXXXXX", NULL);
 	g_free (current_dir);
+
+	g_mkdtemp (tests_data_dir);
 
 	g_test_init (&argc, &argv, NULL);
 
