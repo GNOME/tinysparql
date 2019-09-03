7e9f001a7d5705faab686c8460482b11725d4682
Carlos Garnacho <carlosg@gnome.org>
2019-09-03 22:31:02 +0200

    tests: Add test for GROUP BY (... AS ...)

--- tests/libtracker-data/tracker-sparql-test.c
+++ tests/libtracker-data/tracker-sparql-test.c
@@ -46,6 +46,7 @@ const TestInfo tests[] = {
 	{ "aggregates/aggregate-distinct-1", "aggregates/data-1", FALSE },
 	{ "aggregates/aggregate-group-1", "aggregates/data-1", FALSE },
 	{ "aggregates/aggregate-group-2", "aggregates/data-1", FALSE },
+	{ "aggregates/aggregate-group-as-1", "aggregates/data-1", FALSE },
 	{ "algebra/two-nested-opt", "algebra/two-nested-opt", FALSE },
 	{ "algebra/two-nested-opt-alt", "algebra/two-nested-opt", FALSE },
 	{ "algebra/opt-filter-3", "algebra/opt-filter-3", FALSE },
