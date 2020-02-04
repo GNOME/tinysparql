98fc00ca849e1ede6dfe37067fd281f347efd455
Carlos Garnacho <carlosg@gnome.org>
2020-02-04 23:04:13 +0100

    libtracker-fts: Drop TrackerFTSConfig
    These settings are no longer read through settings, at least from
    library bits.
    
    Since we no longer need the settings, we no longer need the schemas,
    and we don't need the testsuite envvars to have things look into the
    right gschema dirs, and we don't need to compile schemas either.

--- src/libtracker-fts/tracker-fts-tokenizer.c
+++ src/libtracker-fts/tracker-fts-tokenizer.c
@@ -31,7 +31,6 @@
 #include <libtracker-data/tracker-ontologies.h>
 
 #include "tracker-fts-tokenizer.h"
-#include "tracker-fts-config.h"
 #include "fts5.h"
 
 typedef struct TrackerTokenizerData TrackerTokenizerData;
