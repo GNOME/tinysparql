0da4cd399ea9417e90a8290a1c3b0a734284a212
Sam Thursfield <sam@afuera.me.uk>
2020-11-23 20:48:12 +0100

    examples: Move Python query examples to tracker-miners.
    These query the Miner FS, so they should go with it.

--- examples/python/query-sync.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import gi
-from gi.repository import Tracker
-
-conn = Tracker.SparqlConnection.get (None)
-cursor = conn.query ("SELECT ?u WHERE { ?u a nie:InformationElement. }", None)
-
-while (cursor.next (None)):
-    print cursor.get_string (0)
