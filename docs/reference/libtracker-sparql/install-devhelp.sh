1531b05dafd2fc2621ad9f331aa215c82eed688d
Carlos Garnacho <carlosg@gnome.org>
2021-08-19 00:49:32 +0200

    docs: Install devhelp files
    This is very clunky at the moment, since we need to shoehorn our own
    keywords on the devhelp2 file, and install manually.

--- docs/reference/libtracker-sparql/install-devhelp.sh
+++ docs/reference/libtracker-sparql/install-devhelp.sh
@@ -2,11 +2,18 @@
 
 cd ${MESON_BUILD_ROOT}/docs/reference/libtracker-sparql/
 
+docs_name=$1
+echo $docs_name
+docs_path="${docs_name}-doc/devhelp/books/${docs_name}"
+echo $docs_path
+devhelp_file="${docs_path}/*.devhelp2"
+echo $devhelp_file
+
 # Step 1. Build devhelp documentation (we let meson do this)
-# hotdoc run --conf-file tracker-doc.json --devhelp-activate
+# hotdoc run --conf-file '${docs_name}-doc.json' --devhelp-activate
 
 # Step 2. Fix .devhelp2 file so it contains keywords from out ontologies
-cat *doc/devhelp/books/tracker/*.devhelp2 | sed "s/<\/functions>//" - | sed "s/<\/book>//" - >fixed.devhelp2
+cat $devhelp_file | sed "s/<\/functions>//" - | sed "s/<\/book>//" - >fixed.devhelp2
 
 for i in *-ontology.keywords
 do
@@ -14,8 +21,8 @@ do
 done
 
 echo -e "  </functions>\n</book>" >>fixed.devhelp2
-mv fixed.devhelp2 *doc/devhelp/books/tracker/*.devhelp2
+mv fixed.devhelp2 $devhelp_file
 
 # Step 3. Install all files
 mkdir -p ${MESON_INSTALL_PREFIX}/share/devhelp/books
-cp -a *doc/devhelp/books/tracker ${MESON_INSTALL_PREFIX}/share/devhelp/books/
+cp -a $docs_path ${MESON_INSTALL_PREFIX}/share/devhelp/books/
