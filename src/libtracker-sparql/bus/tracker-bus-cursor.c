GNOME/tinysparql

commit 39d54f1c3be2a9b73592c6272e862868dc548d90

libtracker-sparql/bus: Check errors and return values reading cursors
Fixes issues reported by Coverity.

CID: #1518981

src/libtracker-sparql/bus/tracker-bus-cursor.c
@@ -210,16 +210,19 @@ tracker_bus_cursor_next (TrackerSparqlCursor  *cursor,
 
 	g_clear_pointer (&bus_cursor->types, g_free);
 	bus_cursor->types = g_new0 (TrackerSparqlValueType, n_columns);
-	g_input_stream_read_all (G_INPUT_STREAM (bus_cursor->data_stream),
-				 bus_cursor->types,
-				 n_columns * sizeof (gint32),
-				 NULL, NULL, NULL);
+
+	if (!g_input_stream_read_all (G_INPUT_STREAM (bus_cursor->data_stream),
+				      bus_cursor->types,
+				      n_columns * sizeof (gint32),
+				      NULL, NULL, error))
+		return FALSE;
 
 	offsets = g_new0 (gint32, n_columns);
-	g_input_stream_read_all (G_INPUT_STREAM (bus_cursor->data_stream),
-				 offsets,
-				 n_columns * sizeof (gint32),
-				 NULL, NULL, NULL);
+	if (!g_input_stream_read_all (G_INPUT_STREAM (bus_cursor->data_stream),
+				      offsets,
+				      n_columns * sizeof (gint32),
+				      NULL, NULL, error))
+		return FALSE;
 
 	/* The last offset says how long we have to go to read
 	 * the whole row data.

commit 0b183d98349cd1ca89ca1719188234d6d1852f22

libtracker-sparql/bus: Check return value of function
Leftover from the prior fixes when reading cursor data.

Fixes: 39d54f1c3 (libtracker-sparql/bus: Check errors and return...)

src/libtracker-sparql/bus/tracker-bus-cursor.c
@@ -274,10 +274,14 @@ tracker_bus_cursor_next (TrackerSparqlCursor  *cursor,
 	g_clear_pointer (&bus_cursor->row_data, g_free);
 	bus_cursor->row_data =
 		g_new0 (gchar, offsets[n_columns - 1] + 1);
-	g_input_stream_read_all (G_INPUT_STREAM (bus_cursor->data_stream),
-				 bus_cursor->row_data,
-				 offsets[n_columns - 1] + 1,
-				 NULL, NULL, NULL);
+
+	if (!g_input_stream_read_all (G_INPUT_STREAM (bus_cursor->data_stream),
+	                              bus_cursor->row_data,
+	                              offsets[n_columns - 1] + 1,
+	                              NULL, NULL, error)) {
+		g_free (offsets);
+		return FALSE;
+	}
 
 	g_clear_pointer (&bus_cursor->values, g_free);
 	bus_cursor->values = g_new0 (const gchar *, n_columns);