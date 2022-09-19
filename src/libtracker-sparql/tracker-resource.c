cfbcfa116ab4fb63877fe4b6c6205b0880b7a9ba
Carlos Garnacho <carlosg@gnome.org>
2022-09-19 13:27:39 +0200

    libtracker-sparql: Add missing character to IRI escape check
    The backslash character conversion is handled by the function, but
    not properly checked in the opt-out paths. This makes it fail to
    escape IRIs that only have \ as an invalid character.

--- src/libtracker-sparql/tracker-resource.c
+++ src/libtracker-sparql/tracker-resource.c
@@ -103,7 +103,7 @@ escape_iri (const gchar *str)
 
 	/* Fast path, check whether there's no characters to escape */
 	if (!strpbrk (str,
-	              "<>\"{}|^`"
+	              "<>\"{}|^`\\"
 	              "\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10"
 	              "\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20")) {
 		return g_strdup (str);
