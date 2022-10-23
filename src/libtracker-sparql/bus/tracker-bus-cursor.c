GNOME/tinysparql

commit 4098bbda20dac89eefa20840e09673dba4b86862

libtracker-sparql: Reimplement all bus connection objects
Historically the set of objects that provide the bus implementation
were written in Vala, this commit ends with that and rewrites all of
these objects in C. This is mostly a 1:1 port with one exception,
cursors are now based on TrackerDeserializer and input streams, they
were given a memory buffer previously.

The use of Vala here had a few indirect effects:

- Vala code could not be introspected by our gcovr infrastructure for
  code coverage. This made this central piece of our library invisible
  to coverage stats entirely.

- Since our library code and vala contend for the use of visibility
  attributes for public functions, we ended up with a few symbols from
  this Vala code that was never intended to be public API.

- The async code generated by Vala triggers reports about unreachable
  code in Coverity. This has proven very imbued in Vala and hard to
  fix without breaking something else there.

Moving away from Vala fixes all 3. For the sake of maintainability,
this is seen as a net improvement, despite the more verbose code and
the async operation daisy chaning at places. Now all untested code,
leaked symbols and dead code will be a responsibility of our own.

src/libtracker-sparql/bus/tracker-bus-cursor.c
@@ -1,0 +1,361 @@
+/*
+ * Copyright (C) 2010, Nokia <ivan.frade@nokia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+
+#include "config.h"
+
+#include "tracker-bus-cursor.h"
+
+struct _TrackerBusCursor
+{
+	TrackerDeserializer parent_instance;
+	GDataInputStream *data_stream;
+	GVariant *variables;
+	const gchar **variable_names;
+	gint n_columns;
+	TrackerSparqlValueType *types;
+	gchar *row_data;
+	const gchar **values;
+	gboolean finished;
+};
+
+enum {
+	PROP_0,
+	PROP_VARIABLES,
+	N_PROPS
+};
+
+static GParamSpec *props[N_PROPS] = { 0, };
+
+G_DEFINE_TYPE (TrackerBusCursor,
+               tracker_bus_cursor,
+               TRACKER_TYPE_DESERIALIZER)
+
+static void
+tracker_bus_cursor_finalize (GObject *object)
+{
+	TrackerBusCursor *bus_cursor = TRACKER_BUS_CURSOR (object);
+
+	g_clear_object (&bus_cursor->data_stream);
+	g_clear_pointer (&bus_cursor->variables, g_variant_unref);
+	g_clear_pointer (&bus_cursor->types, g_free);
+	g_clear_pointer (&bus_cursor->row_data, g_free);
+	g_clear_pointer (&bus_cursor->values, g_free);
+	g_clear_pointer (&bus_cursor->variable_names, g_free);
+
+	G_OBJECT_CLASS (tracker_bus_cursor_parent_class)->finalize (object);
+}
+
+static void
+tracker_bus_cursor_constructed (GObject *object)
+{
+	TrackerDeserializer *deserializer = TRACKER_DESERIALIZER (object);
+	TrackerBusCursor *cursor = TRACKER_BUS_CURSOR (object);
+
+	G_OBJECT_CLASS (tracker_bus_cursor_parent_class)->constructed (object);
+
+	cursor->variable_names = g_variant_get_strv (cursor->variables, NULL);
+	cursor->n_columns = g_strv_length ((gchar **) cursor->variable_names);
+	cursor->data_stream =
+		g_data_input_stream_new (tracker_deserializer_get_stream (deserializer));
+	g_data_input_stream_set_byte_order (cursor->data_stream,
+					    G_DATA_STREAM_BYTE_ORDER_HOST_ENDIAN);
+}
+
+static void
+tracker_bus_cursor_set_property (GObject      *object,
+				 guint         prop_id,
+				 const GValue *value,
+				 GParamSpec   *pspec)
+{
+	TrackerBusCursor *cursor = TRACKER_BUS_CURSOR (object);
+
+	switch (prop_id) {
+	case PROP_VARIABLES:
+		cursor->variables = g_value_dup_variant (value);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+		break;
+	}
+}
+
+static void
+tracker_bus_cursor_get_property (GObject    *object,
+				 guint       prop_id,
+				 GValue     *value,
+				 GParamSpec *pspec)
+{
+	TrackerBusCursor *cursor = TRACKER_BUS_CURSOR (object);
+
+	switch (prop_id) {
+	case PROP_VARIABLES:
+		g_value_set_variant (value, cursor->variables);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+		break;
+	}
+}
+
+static gint
+tracker_bus_cursor_get_n_columns (TrackerSparqlCursor *cursor)
+{
+	TrackerBusCursor *bus_cursor = TRACKER_BUS_CURSOR (cursor);
+
+	return bus_cursor->n_columns;
+}
+
+static TrackerSparqlValueType
+tracker_bus_cursor_get_value_type (TrackerSparqlCursor *cursor,
+                                   gint                 column)
+{
+	TrackerBusCursor *bus_cursor = TRACKER_BUS_CURSOR (cursor);
+
+	if (bus_cursor->finished)
+		return TRACKER_SPARQL_VALUE_TYPE_UNBOUND;
+	if (column < 0 || column >= bus_cursor->n_columns)
+		return TRACKER_SPARQL_VALUE_TYPE_UNBOUND;
+	if (!bus_cursor->types)
+		return TRACKER_SPARQL_VALUE_TYPE_UNBOUND;
+
+	return bus_cursor->types[column];
+}
+
+static const gchar *
+tracker_bus_cursor_get_variable_name (TrackerSparqlCursor *cursor,
+                                      gint                 column)
+{
+	TrackerBusCursor *bus_cursor = TRACKER_BUS_CURSOR (cursor);
+
+	if (bus_cursor->finished)
+		return NULL;
+	if (column < 0 || column >= bus_cursor->n_columns)
+		return NULL;
+
+	return bus_cursor->variable_names[column];
+}
+
+static const gchar *
+tracker_bus_cursor_get_string (TrackerSparqlCursor *cursor,
+                               gint                 column,
+			       glong               *len)
+{
+	TrackerBusCursor *bus_cursor = TRACKER_BUS_CURSOR (cursor);
+	const gchar *str;
+
+	if (bus_cursor->finished)
+		return NULL;
+	if (column < 0 || column >= bus_cursor->n_columns)
+		return NULL;
+	if (!bus_cursor->types)
+		return NULL;
+
+	/* Return null instead of empty string for unbound values */
+	if (bus_cursor->types[column] == TRACKER_SPARQL_VALUE_TYPE_UNBOUND)
+		return NULL;
+
+	str = bus_cursor->values[column];
+
+	if (len)
+		*len = strlen (str);
+
+	return str;
+}
+
+static gboolean
+tracker_bus_cursor_next (TrackerSparqlCursor  *cursor,
+                         GCancellable         *cancellable,
+                         GError              **error)
+{
+	TrackerBusCursor *bus_cursor = TRACKER_BUS_CURSOR (cursor);
+	gint32 *offsets, n_columns, i;
+
+	if (bus_cursor->finished)
+		return FALSE;
+	if (g_cancellable_set_error_if_cancelled (cancellable, error))
+		return FALSE;
+
+	/* So, the make up on each cursor segment is:
+	 *
+	 * iteration = [4 bytes for number of columns,
+	 *              columns x 4 bytes for types
+	 *              columns x 4 bytes for offsets]
+	 */
+	n_columns = g_data_input_stream_read_int32 (bus_cursor->data_stream,
+						    NULL, NULL);
+
+	if (n_columns == 0) {
+		bus_cursor->finished = TRUE;
+		return FALSE;
+	}
+
+	g_clear_pointer (&bus_cursor->types, g_free);
+	bus_cursor->types = g_new0 (TrackerSparqlValueType, n_columns);
+	g_input_stream_read_all (G_INPUT_STREAM (bus_cursor->data_stream),
+				 bus_cursor->types,
+				 n_columns * sizeof (gint32),
+				 NULL, NULL, NULL);
+
+	offsets = g_new0 (gint32, n_columns);
+	g_input_stream_read_all (G_INPUT_STREAM (bus_cursor->data_stream),
+				 offsets,
+				 n_columns * sizeof (gint32),
+				 NULL, NULL, NULL);
+
+	/* The last offset says how long we have to go to read
+	 * the whole row data.
+	 */
+	g_clear_pointer (&bus_cursor->row_data, g_free);
+	bus_cursor->row_data =
+		g_new0 (gchar, offsets[n_columns - 1] + 1);
+	g_input_stream_read_all (G_INPUT_STREAM (bus_cursor->data_stream),
+				 bus_cursor->row_data,
+				 offsets[n_columns - 1] + 1,
+				 NULL, NULL, NULL);
+
+	g_clear_pointer (&bus_cursor->values, g_free);
+	bus_cursor->values = g_new0 (const gchar *, n_columns);
+
+	for (i = 0; i < n_columns; i++) {
+		if (i == 0)
+			bus_cursor->values[i] = bus_cursor->row_data;
+		else
+			bus_cursor->values[i] = &bus_cursor->row_data[offsets[i - 1]] + 1;
+	}
+
+	g_free (offsets);
+
+	return TRUE;
+}
+
+static void
+next_in_thread (GTask        *task,
+		gpointer      source_object,
+		gpointer      task_data,
+		GCancellable *cancellable)
+{
+	GError *error = NULL;
+	gboolean retval;
+
+	retval = tracker_sparql_cursor_next (source_object, cancellable, &error);
+
+	if (error)
+		g_task_return_error (task, error);
+	else
+		g_task_return_boolean (task, retval);
+}
+
+static void
+tracker_bus_cursor_next_async (TrackerSparqlCursor  *cursor,
+                               GCancellable         *cancellable,
+                               GAsyncReadyCallback   cb,
+                               gpointer              user_data)
+{
+	GTask *task;
+
+	task = g_task_new (cursor, cancellable, cb, user_data);
+	g_task_run_in_thread (task, next_in_thread);
+}
+
+static gboolean
+tracker_bus_cursor_next_finish (TrackerSparqlCursor  *cursor,
+                                GAsyncResult         *res,
+                                GError              **error)
+{
+	return g_task_propagate_boolean (G_TASK (res), error);
+}
+
+static void
+tracker_bus_cursor_rewind (TrackerSparqlCursor *cursor)
+{
+	TrackerBusCursor *bus_cursor = TRACKER_BUS_CURSOR (cursor);
+
+	g_clear_pointer (&bus_cursor->types, g_free);
+	g_clear_pointer (&bus_cursor->row_data, g_free);
+
+	if (g_seekable_can_seek (G_SEEKABLE (bus_cursor->data_stream))) {
+		g_seekable_seek (G_SEEKABLE (bus_cursor->data_stream),
+				 0, G_SEEK_SET, NULL, NULL);
+	}
+
+	bus_cursor->finished = FALSE;
+}
+
+static void
+tracker_bus_cursor_close (TrackerSparqlCursor *cursor)
+{
+	TrackerBusCursor *bus_cursor = TRACKER_BUS_CURSOR (cursor);
+
+	g_input_stream_close (G_INPUT_STREAM (bus_cursor->data_stream),
+			      NULL, NULL);
+
+	TRACKER_SPARQL_CURSOR_CLASS (tracker_bus_cursor_parent_class)->close (cursor);
+}
+
+static void
+tracker_bus_cursor_class_init (TrackerBusCursorClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+	TrackerSparqlCursorClass *cursor_class =
+		TRACKER_SPARQL_CURSOR_CLASS (klass);
+
+	object_class->finalize = tracker_bus_cursor_finalize;
+	object_class->constructed = tracker_bus_cursor_constructed;
+	object_class->set_property = tracker_bus_cursor_set_property;
+	object_class->get_property = tracker_bus_cursor_get_property;
+
+	cursor_class->get_n_columns = tracker_bus_cursor_get_n_columns;
+	cursor_class->get_value_type = tracker_bus_cursor_get_value_type;
+	cursor_class->get_variable_name = tracker_bus_cursor_get_variable_name;
+	cursor_class->get_string = tracker_bus_cursor_get_string;
+	cursor_class->next = tracker_bus_cursor_next;
+	cursor_class->next_async = tracker_bus_cursor_next_async;
+	cursor_class->next_finish = tracker_bus_cursor_next_finish;
+	cursor_class->rewind = tracker_bus_cursor_rewind;
+	cursor_class->close = tracker_bus_cursor_close;
+
+	props[PROP_VARIABLES] =
+		g_param_spec_variant ("variables",
+				      "Variables",
+				      "Variables",
+				      G_VARIANT_TYPE ("as"),
+				      NULL,
+				      G_PARAM_READWRITE |
+				      G_PARAM_STATIC_STRINGS |
+				      G_PARAM_CONSTRUCT_ONLY);
+
+	g_object_class_install_properties (object_class, N_PROPS, props);
+
+}
+
+static void
+tracker_bus_cursor_init (TrackerBusCursor *cursor)
+{
+}
+
+TrackerSparqlCursor *
+tracker_bus_cursor_new (GInputStream *stream,
+			GVariant     *variables)
+
+{
+	return g_object_new (TRACKER_TYPE_BUS_CURSOR,
+	                     "stream", stream,
+	                     "variables", variables,
+	                     NULL);
+}

src/libtracker-common/libtracker-common.vapi
@@ -1,120 +1,0 @@
-/*
- * Copyright (C) 2008-2009, Nokia
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA  02110-1301, USA.
- */
-
-[CCode (cprefix = "Tracker", gir_namespace = "Tracker",
-        gir_version = "2.0", lower_case_cprefix = "tracker_")]
-namespace Tracker {
-	[CCode (cheader_filename = "libtracker-common/tracker-date-time.h")]
-	public double string_to_date (string date_string, out int offset) throws DateError;
-
-	[CCode (cheader_filename = "libtracker-common/tracker-utils.h")]
-	public string? util_build_dbus_uri (GLib.BusType bus_type, string service, string? path);
-
-	[CCode (cheader_filename = "libtracker-common/tracker-date-time.h")]
-	public errordomain DateError {
-		OFFSET,
-		INVALID_ISO8601
-	}
-
-	[CCode (cheader_filename = "libtracker-common/tracker-common.h")]
-	public class ConfigFile : GLib.Object {
-		[CCode (has_construct_function = false)]
-		public ConfigFile ();
-		[NoAccessorMethod]
-		public string domain { get; construct; }
-		public bool save ();
-		public virtual signal void changed ();
-		public GLib.File file;
-		public GLib.FileMonitor monitor;
-		public bool file_exists;
-		public GLib.KeyFile key_file;
-	}
-
-	[CCode (cheader_filename = "libtracker-common/tracker-common.h")]
-	public class KeyfileObject {
-		public static string blurb (void *object, string property);
-		public static bool default_boolean (void *object, string property);
-		public static int  default_int (void *object, string property);
-		public static bool validate_int (void *object, string propery, int value);
-		public static void load_int (void *object, string property, GLib.KeyFile key_file, string group, string key);
-		public static void load_boolean (void *object, string property, GLib.KeyFile key_file, string group, string key);
-		public static void load_string (void *object, string property, GLib.KeyFile key_file, string group, string key);
-		public static void load_string_list (void *object, string property, GLib.KeyFile key_file, string group, string key, out GLib.SList return_instead);
-		public static void load_directory_list (void *object, string property, GLib.KeyFile key_file, string group, string key, bool is_recursive, out GLib.SList return_instead);
-		public static void save_int (void *object, string property, GLib.KeyFile key_file, string group, string key);
-		public static void save_boolean (void *object, string property, GLib.KeyFile key_file, string group, string key);
-		public static void save_string (void *object, string property, GLib.KeyFile key_file, string group, string key);
-		public static void save_string_list (void *object, string property, GLib.KeyFile key_file, string group, string key);
-		public static void save_directory_list (void *object, string property, GLib.KeyFile key_file, string group, string key);
-	}
-
-	[CCode (cheader_filename = "libtracker-common/tracker-locale.h")]
-	namespace Locale {
-		public void sanity_check ();
-	}
-
-	[Compact]
-	[CCode (cheader_filename = "libtracker-common/tracker-common.h")]
-	namespace IPC {
-		public GLib.BusType bus ();
-	}
-
-	[Compact]
-	[CCode (ref_function = "", unref_function = "", cheader_filename = "libtracker-common/tracker-common.h")]
-	public class DBusRequest {
-		public static DBusRequest begin (string? sender, string format,...);
-		public void debug (string format,...);
-		public void end (GLib.Error? e = null);
-		[CCode (cname = "tracker_dbus_enable_client_lookup")]
-		public static void enable_client_lookup (bool enable);
-	}
-
-	[CCode (ref_function = "tracker_domain_ontology_ref", unref_function = "tracker_domain_ontology_unref", cheader_filename = "libtracker-common/tracker-domain-ontology.h")]
-	public class DomainOntology {
-		public DomainOntology (string? name, GLib.Cancellable? cancellable) throws GLib.Error;
-		public GLib.File get_cache ();
-		public GLib.File? get_journal ();
-		public GLib.File get_ontology ();
-		public string get_domain (string? suffix = null);
-	}
-
-	[CCode (cheader_filename = "libtracker-common/tracker-common.h")]
-	public void ioprio_init ();
-
-	[CCode (cheader_filename = "libtracker-common/tracker-debug.h")]
-	public uint get_debug_flags ();
-
-	[CCode (cname = "g_message", cheader_filename = "glib.h")]
-	[PrintfFormat]
-	public void message (string format, ...);
-	[CCode (cname = "g_warning", cheader_filename = "glib.h")]
-	[PrintfFormat]
-	public void warning (string format, ...);
-	[CCode (cname = "g_critical", cheader_filename = "glib.h")]
-	[PrintfFormat]
-	public void critical (string format, ...);
-	[CCode (cname = "g_error", cheader_filename = "glib.h")]
-	[PrintfFormat]
-	[NoReturn]
-	public void error (string format, ...);
-	[CCode (cname = "g_debug", cheader_filename = "glib.h")]
-	[PrintfFormat]
-	public void debug (string format, ...);
-}
-

src/libtracker-sparql/bus/.gitignore
@@ -1,5 +1,0 @@
-tracker-namespace.c
-tracker-bus.[ch]
-tracker-bus*.vapi
-tracker-array-cursor.c
-tracker-bus-fd-cursor.c

src/libtracker-sparql/bus/meson.build
@@ -1,7 +1,6 @@
 bus_files = files(
-    'tracker-bus.vala',
-    'tracker-namespace.vala',
-    'tracker-bus-batch.vala',
-    'tracker-bus-fd-cursor.vala',
-    'tracker-bus-statement.vala',
+    'tracker-bus.c',
+    'tracker-bus-batch.c',
+    'tracker-bus-cursor.c',
+    'tracker-bus-statement.c',
 )

src/libtracker-sparql/bus/tracker-bus-batch.c
@@ -1,0 +1,192 @@
+/*
+ * Copyright (C) 2021, Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ *
+ * Author: Carlos Garnacho <carlosg@gnome.org>
+ */
+
+#include "config.h"
+
+#include "tracker-bus-batch.h"
+
+struct _TrackerBusBatch
+{
+	TrackerBatch parent_instance;
+	GPtrArray *updates;
+};
+
+typedef struct {
+	GMainLoop *loop;
+	gboolean retval;
+	GError *error;
+} ExecuteAsyncData;
+
+G_DEFINE_TYPE (TrackerBusBatch, tracker_bus_batch, TRACKER_TYPE_BATCH)
+
+static void
+tracker_bus_batch_finalize (GObject *object)
+{
+	TrackerBusBatch *bus_batch = TRACKER_BUS_BATCH (object);
+
+	g_ptr_array_unref (bus_batch->updates);
+
+	G_OBJECT_CLASS (tracker_bus_batch_parent_class)->finalize (object);
+}
+
+static void
+tracker_bus_batch_add_sparql (TrackerBatch *batch,
+                              const gchar  *sparql)
+{
+	TrackerBusBatch *bus_batch = TRACKER_BUS_BATCH (batch);
+
+	g_ptr_array_add (bus_batch->updates, g_strdup (sparql));
+}
+
+static void
+tracker_bus_batch_add_resource (TrackerBatch    *batch,
+                                const gchar     *graph,
+                                TrackerResource *resource)
+{
+	TrackerBusBatch *bus_batch = TRACKER_BUS_BATCH (batch);
+	TrackerSparqlConnection *conn;
+	TrackerNamespaceManager *namespaces;
+	gchar *sparql;
+
+	conn = tracker_batch_get_connection (batch);
+	namespaces = tracker_sparql_connection_get_namespace_manager (conn);
+	sparql = tracker_resource_print_sparql_update (resource, namespaces, graph);
+	g_ptr_array_add (bus_batch->updates, sparql);
+}
+
+static void
+execute_cb (GObject      *source,
+            GAsyncResult *res,
+            gpointer      user_data)
+{
+	ExecuteAsyncData *data = user_data;
+
+	data->retval =
+		tracker_batch_execute_finish (TRACKER_BATCH (source),
+		                              res, &data->error);
+	g_main_loop_quit (data->loop);
+}
+
+static gboolean
+tracker_bus_batch_execute (TrackerBatch  *batch,
+                           GCancellable  *cancellable,
+                           GError       **error)
+{
+	GMainContext *context;
+	ExecuteAsyncData data = { 0, };
+
+	context = g_main_context_new ();
+	data.loop = g_main_loop_new (context, FALSE);
+	g_main_context_push_thread_default (context);
+
+	tracker_batch_execute_async (batch,
+				     cancellable,
+				     execute_cb,
+				     &data);
+
+	g_main_loop_run (data.loop);
+
+	g_main_context_pop_thread_default (context);
+
+	g_main_loop_unref (data.loop);
+
+	if (data.error) {
+		g_propagate_error (error, data.error);
+		return FALSE;
+	}
+
+	return data.retval;
+}
+
+static void
+update_array_cb (GObject      *source,
+                 GAsyncResult *res,
+                 gpointer      user_data)
+{
+	GTask *task = user_data;
+	GError *error = NULL;
+
+	if (!tracker_sparql_connection_update_array_finish (TRACKER_SPARQL_CONNECTION (source),
+	                                                    res, &error))
+		g_task_return_error (task, error);
+	else
+		g_task_return_boolean (task, TRUE);
+
+	g_object_unref (task);
+}
+
+static void
+tracker_bus_batch_execute_async (TrackerBatch        *batch,
+                                 GCancellable        *cancellable,
+                                 GAsyncReadyCallback  callback,
+                                 gpointer             user_data)
+{
+	TrackerBusBatch *bus_batch = TRACKER_BUS_BATCH (batch);
+	TrackerSparqlConnection *conn;
+	GTask *task;
+
+	task = g_task_new (batch, cancellable, callback, user_data);
+	conn = tracker_batch_get_connection (batch);
+	tracker_sparql_connection_update_array_async (conn,
+	                                              (gchar **) bus_batch->updates->pdata,
+	                                              bus_batch->updates->len,
+	                                              cancellable,
+	                                              update_array_cb,
+	                                              task);
+}
+
+static gboolean
+tracker_bus_batch_execute_finish (TrackerBatch  *batch,
+                                  GAsyncResult  *res,
+                                  GError       **error)
+{
+	return g_task_propagate_boolean (G_TASK (res), error);
+}
+
+static void
+tracker_bus_batch_class_init (TrackerBusBatchClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+	TrackerBatchClass *batch_class = TRACKER_BATCH_CLASS (klass);
+
+	object_class->finalize = tracker_bus_batch_finalize;
+
+	batch_class->add_sparql = tracker_bus_batch_add_sparql;
+	batch_class->add_resource = tracker_bus_batch_add_resource;
+	batch_class->execute = tracker_bus_batch_execute;
+	batch_class->execute_async = tracker_bus_batch_execute_async;
+	batch_class->execute_finish = tracker_bus_batch_execute_finish;
+}
+
+static void
+tracker_bus_batch_init (TrackerBusBatch *batch)
+{
+	batch->updates = g_ptr_array_new ();
+	g_ptr_array_set_free_func (batch->updates, g_free);
+}
+
+TrackerBatch *
+tracker_bus_batch_new (TrackerBusConnection *connection)
+{
+	return g_object_new (TRACKER_TYPE_BUS_BATCH,
+	                     "connection", connection,
+	                     NULL);
+}

src/libtracker-sparql/bus/tracker-bus-batch.h
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2021, Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ *
+ * Author: Carlos Garnacho <carlosg@gnome.org>
+ */
+
+#ifndef __TRACKER_BUS_BATCH_H__
+#define __TRACKER_BUS_BATCH_H__
+
+#include "tracker-bus.h"
+
+#include <libtracker-sparql/tracker-private.h>
+
+#define TRACKER_TYPE_BUS_BATCH (tracker_bus_batch_get_type ())
+G_DECLARE_FINAL_TYPE (TrackerBusBatch,
+                      tracker_bus_batch,
+                      TRACKER, BUS_BATCH,
+                      TrackerBatch)
+
+TrackerBatch * tracker_bus_batch_new (TrackerBusConnection *connection);
+
+#endif /* __TRACKER_BUS_BATCH_H__ */

src/libtracker-sparql/bus/tracker-bus-batch.vala
@@ -1,63 +1,0 @@
-/*
- * Copyright (C) 2021, Red Hat Inc.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA  02110-1301, USA.
- *
- * Author: Carlos Garnacho <carlosg@gnome.org>
- */
-
-public class Tracker.Bus.Batch : Tracker.Batch {
-	private DBusConnection bus;
-	private string dbus_name;
-	private string object_path;
-	private string[] updates;
-
-	public Batch (DBusConnection bus, string dbus_name, string object_path) {
-		Object ();
-		this.bus = bus;
-		this.dbus_name = dbus_name;
-		this.object_path = object_path;
-	}
-
-	public override void add_sparql (string sparql) {
-		updates += sparql;
-	}
-
-	public override void add_resource (string? graph, Resource resource) {
-		var namespaces = this.connection.get_namespace_manager ();
-		var sparql = resource.print_sparql_update (namespaces, graph);
-		updates += sparql;
-	}
-
-	public override bool execute (GLib.Cancellable? cancellable) throws Sparql.Error, GLib.Error, GLib.IOError, GLib.DBusError {
-		// use separate main context for sync operation
-		var context = new MainContext ();
-		var loop = new MainLoop (context, false);
-		context.push_thread_default ();
-		AsyncResult async_res = null;
-		execute_async.begin (cancellable, (o, res) => {
-			async_res = res;
-			loop.quit ();
-		});
-		loop.run ();
-		context.pop_thread_default ();
-		return execute_async.end (async_res);
-	}
-
-	public async override bool execute_async (GLib.Cancellable? cancellable) throws Sparql.Error, GLib.Error, GLib.IOError, GLib.DBusError {
-		return yield Tracker.Bus.Connection.perform_update_array (bus, dbus_name, object_path, updates, cancellable);
-	}
-}

src/libtracker-sparql/bus/tracker-bus-cursor.h
@@ -1,0 +1,33 @@
+/*
+ * Copyright (C) 2010, Nokia <ivan.frade@nokia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+#ifndef __TRACKER_BUS_CURSOR_H__
+#define __TRACKER_BUS_CURSOR_H__
+
+#include <libtracker-sparql/tracker-sparql.h>
+
+#define TRACKER_TYPE_BUS_CURSOR (tracker_bus_cursor_get_type ())
+G_DECLARE_FINAL_TYPE (TrackerBusCursor,
+                      tracker_bus_cursor,
+                      TRACKER, BUS_CURSOR,
+                      TrackerDeserializer);
+
+TrackerSparqlCursor *tracker_bus_cursor_new (GInputStream *stream,
+					     GVariant     *variables);
+
+#endif /* __TRACKER_BUS_CURSOR_H__ */

commit 8caded76418d6e87d70699197784fef0a6a36c32

libtracker-sparql/bus: Plug GTask leak
The task was not being unreferenced after dispatching, thus leaking
a reference on its cursor source object, thus preventing it from
being implicitly closed if the application does not do an explicit
tracker_sparql_cursor_close().

Fixes: 4098bbda20 (libtracker-sparql: Reimplement all bus connection objects)
Closes: https://gitlab.gnome.org/GNOME/tracker/-/issues/383

src/libtracker-sparql/bus/tracker-bus-cursor.c
@@ -325,6 +325,7 @@ tracker_bus_cursor_next_async (TrackerSparqlCursor  *cursor,
 
 	task = g_task_new (cursor, cancellable, cb, user_data);
 	g_task_run_in_thread (task, next_in_thread);
+	g_object_unref (task);
 }
 
 static gboolean