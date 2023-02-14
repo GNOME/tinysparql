GNOME/tinysparql

commit c771c5113b94062b571823d872eaabb967367142

trackertestutils: Create DConf profile on startup
We must use a separate DConf profile during tests, so we can set
GSettings values without messing up the user's actual system config.

utils/trackertestutils/dconf.py
@@ -18,9 +18,13 @@
 # 02110-1301, USA.
 #
 
+from pathlib import Path
+import atexit
 import logging
 import os
+import shutil
 import subprocess
+import tempfile
 
 log = logging.getLogger(__name__)
 
@@ -39,10 +43,23 @@ class DConfClient(object):
     environment.
     """
 
-    def __init__(self, sandbox):
+    def __init__(self, extra_env, session_bus_address):
         self.env = os.environ
-        self.env.update(sandbox.extra_env)
-        self.env['DBUS_SESSION_BUS_ADDRESS'] = sandbox.get_session_bus_address()
+        self.env.update(extra_env)
+        self.env['DBUS_SESSION_BUS_ADDRESS'] = session_bus_address
+        self.env['DCONF_PROFILE'] = self._create_dconf_profile()
+        self._check_using_correct_dconf_profile()
+
+    def _create_dconf_profile(self) -> str:
+        self._tmpdir = tempfile.TemporaryDirectory()
+        atexit.register(self._remove_dconf_profile)
+
+        profile = Path(self._tmpdir.name).joinpath('trackertest')
+        profile.write_text('user-db:trackertest')
+        return str(profile)
+
+    def _remove_dconf_profile(self):
+        shutil.rmtree(self._tmpdir)
 
     def _check_using_correct_dconf_profile(self):
         profile = self.env.get("DCONF_PROFILE")
@@ -52,10 +69,7 @@ class DConfClient(object):
                 "be created inside a TrackerDBussandbox to avoid risk of "
                 "interfering with real settings.")
         if not os.path.exists(profile):
-            raise Exception(
-                "Unable to find DConf profile '%s'. Check that Tracker and "
-                "the test suite have been correctly installed (you must pass "
-                "--enable-functional-tests to configure)." % profile)
+            raise Exception("Unable to find DConf profile '%s'." % profile)
 
         assert os.path.basename(profile) == "trackertest"
 

utils/trackertestutils/__init__.py
@@ -1,1 +1,0 @@
-

utils/trackertestutils/__main__.py
@@ -142,7 +142,7 @@ def create_sandbox(store_location, prefix=None, use_session_dirs=False,
 
 def config_set(sandbox, content_locations_recursive=None,
                content_locations_single=None, applications=False):
-    dconfclient = dconf.DConfClient(sandbox)
+    dconfclient = sandbox.get_dconf_client()
 
     if content_locations_recursive:
         log.debug("Using content locations: %s" %

utils/trackertestutils/sandbox.py
@@ -72,6 +72,8 @@ class TrackerSandbox:
         else:
             self.system_bus = None
 
+        self._dconf_client = None
+
     def get_environment(self):
         env = os.environ
         env.update(self.extra_env)
@@ -163,6 +165,13 @@ class TrackerSandbox:
     def get_session_bus_address(self):
         return self.session_bus.get_address()
 
+    def get_dconf_client(self):
+        if self._dconf_client is None:
+            self._dconf_client = dconf.DConfClient(
+                self.extra_env, self.get_session_bus_address()
+            )
+        return self._dconf_client
+
     def set_config(self, schema_config_dict):
         """Set config values in multiple GSettings schemas.
 
@@ -176,7 +185,7 @@ class TrackerSandbox:
 
         """
 
+        dconf_client = self.get_dconf_client()
         for schema_name, contents in schema_config_dict.items():
-            dconfclient = dconf.DConfClient(self)
             for key, value in contents.items():
-                dconfclient.write(schema_name, key, value)
+                dconf_client.write(schema_name, key, value)

commit 7a72bf9b2829c8c95ec35b087743760754d821e4

utils: Delete dconf temporary directory with TemporaryDirectory.cleanup()
The function rmtree does not take a TemporaryDirectory object, but we
don't need it anyway. Use the TemporaryDirectory cleanup() method to
delete directory content.

Fixes: c771c5113 - trackertestutils: Create DConf profile on startup

utils/trackertestutils/dconf.py
@@ -59,7 +59,7 @@ class DConfClient(object):
         return str(profile)
 
     def _remove_dconf_profile(self):
-        shutil.rmtree(self._tmpdir)
+        self._tmpdir.cleanup()
 
     def _check_using_correct_dconf_profile(self):
         profile = self.env.get("DCONF_PROFILE")