GNOME/tinysparql

commit f436db08b43461db21d75ddfe6422b017364c043

core: Avoid flushing updates within delete+insert+where queries
Currently, we perform the insert/delete operations while iterating
a cursor dealing with the where clause. Performing updates while
performing select queries and iterating through cursors is usually
not an issue, but that is only true (with WAL) if this is performed
through different interfaces, so the readers can interact with the
unmodified database while updates are accumulated in the write-ahead
log. More information at https://www.sqlite.org/isolation.html.

But here, we are dealing with a cursor created from the update thread,
inside the same lock and seeing the same data than the latest update.

So, if a delete{}insert{}where{} query happens to be modifying data
that alter the results of the where{} clause, we might be getting the
cursor silently updated underneath, thus either missing data that
originally matched the where{} clause, or overreaching with brand
new data that happens to match the where{} clause.

As per the specs, the execution order is executing the where{}, obtaining
resultset, then apply delete+insert clauses on each result. We cannot
let these updates happen while iterating the resultset, so freeze/thaw
the flush mechanism to ensure that these update operations are accumulated
without effects on the database. After all the results are iterated, the
accumulated operations will be flushed at once.

Related: https://gitlab.gnome.org/GNOME/tracker-miners/-/issues/327

src/libtracker-sparql/core/tracker-sparql.c
@@ -10344,6 +10344,7 @@ apply_update (TrackerSparql    *sparql,
               GError          **error)
 {
 	GHashTable *updated_bnode_labels, *bnode_rowids;
+	TrackerDBInterface *iface;
 	GError *inner_error = NULL;
 	guint cur_update_group = 0;
 	guint i = 0;
@@ -10356,6 +10357,8 @@ apply_update (TrackerSparql    *sparql,
 			                       g_free,
 			                       (GDestroyNotify) tracker_rowid_free);
 	}
+
+	iface = tracker_data_manager_get_writable_db_interface (sparql->data_manager);
 
 	updated_bnode_labels = g_hash_table_new (g_str_hash, g_str_equal);
 	bnode_rowids = g_hash_table_new_full (tracker_rowid_hash,
@@ -10382,7 +10385,6 @@ apply_update (TrackerSparql    *sparql,
 		g_hash_table_remove_all (updated_bnode_labels);
 
 		if (update_group->where_clause_sql) {
-			TrackerDBInterface *iface;
 			TrackerDBStatement *stmt;
 
 			/* Flush to ensure the WHERE clause gets up-to-date results */
@@ -10391,7 +10393,8 @@ apply_update (TrackerSparql    *sparql,
 			if (inner_error)
 				goto out;
 
-			iface = tracker_data_manager_get_writable_db_interface (sparql->data_manager);
+			tracker_data_update_freeze_flush (tracker_data_manager_get_data (sparql->data_manager));
+
 			stmt = prepare_query (sparql, iface,
 			                      update_group->where_clause_sql,
 			                      update_group->literals,
@@ -10436,9 +10439,14 @@ apply_update (TrackerSparql    *sparql,
 
 		g_clear_object (&cursor);
 
-		if (!inner_error)
+		if (!inner_error && update_group->where_clause_sql) {
+			tracker_data_update_thaw_flush (tracker_data_manager_get_data (sparql->data_manager));
+		}
+
+		if (!inner_error) {
 			tracker_data_update_buffer_flush (tracker_data_manager_get_data (sparql->data_manager),
 			                                  &inner_error);
+		}
 
 		if (inner_error)
 			goto out;

commit 36e744021ec6e868ad158a8b7e0cd884077b340e

core: Avoid flush prevention on INSERT OR REPLACE {} WHERE {}
This mechanism was added so that DELETE{}INSERT{}WHERE{} updates
did not change the resultset as it was being iterated, but it
missed that these paths are somewhat shared with our custom
INSERT OR REPLACE syntax (if used together with a WHERE clause)
this resulted in these update queries possibly missing some of
their updates.

Change these paths to explicitly avoid this INSERT OR REPLACE
syntax (handled through the TRACKER_UPDATE_UPDATE update type)
so that changes are flushed when necessary for the later queries
to find.

Fixes: f436db08b ("core: Avoid flushing updates within delete+insert+where queries")
Closes: https://gitlab.gnome.org/GNOME/gnome-music/-/issues/618

src/libtracker-sparql/core/tracker-sparql.c
@@ -10369,6 +10369,7 @@ apply_update (TrackerSparql    *sparql,
 	while (i < sparql->update_ops->len) {
 		TrackerUpdateOpGroup *update_group;
 		TrackerSparqlCursor *cursor = NULL;
+		gboolean freeze_flush = FALSE;
 		guint j;
 
 		g_assert (cur_update_group < sparql->update_groups->len);
@@ -10386,6 +10387,7 @@ apply_update (TrackerSparql    *sparql,
 
 		if (update_group->where_clause_sql) {
 			TrackerDBStatement *stmt;
+			TrackerUpdateOp  *op;
 
 			/* Flush to ensure the WHERE clause gets up-to-date results */
 			tracker_data_update_buffer_flush (tracker_data_manager_get_data (sparql->data_manager),
@@ -10393,7 +10395,13 @@ apply_update (TrackerSparql    *sparql,
 			if (inner_error)
 				goto out;
 
-			tracker_data_update_freeze_flush (tracker_data_manager_get_data (sparql->data_manager));
+			op = &g_array_index (sparql->update_ops,
+			                     TrackerUpdateOp,
+			                     update_group->start_idx);
+			freeze_flush = (op->update_type != TRACKER_UPDATE_UPDATE);
+
+			if (freeze_flush)
+				tracker_data_update_freeze_flush (tracker_data_manager_get_data (sparql->data_manager));
 
 			stmt = prepare_query (sparql, iface,
 			                      update_group->where_clause_sql,
@@ -10439,7 +10447,7 @@ apply_update (TrackerSparql    *sparql,
 
 		g_clear_object (&cursor);
 
-		if (!inner_error && update_group->where_clause_sql) {
+		if (!inner_error && freeze_flush) {
 			tracker_data_update_thaw_flush (tracker_data_manager_get_data (sparql->data_manager));
 		}
 