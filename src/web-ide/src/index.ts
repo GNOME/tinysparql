12a6f40fbd325321dc782e672755fdd9eb02faf5
rachleona <racheltam818@icloud.com>
2024-07-04 22:29:39 +0100

    web-ide:  Added bulma to frontend

--- src/web-ide/src/index.ts
+++ src/web-ide/src/index.ts
@@ -1,6 +1,8 @@
 import {EditorView, basicSetup} from 'codemirror';
 import { sparql } from 'codemirror-lang-sparql';
 import run from './run';
+import './style.scss';
+require('./assets/favicon.ico');
 
 const fixedHeightEditor = EditorView.theme({
     "&": {height: "500px"},
--- src/web-ide/webpack.config.js
+++ src/web-ide/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -18,8 +19,11 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        title: 'TinySPARQL web-IDE',
-        template: './src/index.html',
+      title: 'TinySPARQL web-IDE',
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
     })
   ],
   module: {
@@ -32,6 +36,21 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.scss$/,
+        use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+      }
     ],
   },
   resolve: {
