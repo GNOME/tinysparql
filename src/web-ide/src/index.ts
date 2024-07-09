c79deacca49895b0f545e2cc6ad911a0b661f724
rachleona <racheltam818@icloud.com>
2024-07-09 18:06:03 +0100

    web-ide: Basic colour theming

--- src/web-ide/src/index.ts
+++ src/web-ide/src/index.ts
@@ -1,17 +1,37 @@
 import {EditorView, basicSetup} from 'codemirror';
 import { sparql } from 'codemirror-lang-sparql';
+import {tags} from "@lezer/highlight";
+import {HighlightStyle, syntaxHighlighting} from "@codemirror/language";
+
 import run from './run';
 import './style.scss';
 import './assets/favicon.ico';
 
 const fixedHeightEditor = EditorView.theme({
     "&": {height: "40vh"},
-    ".cm-scroller": {overflow: "auto"}
-})
+    ".cm-scroller": {overflow: "auto"},
+    "&.cm-focused .cm-cursor": { borderLeftColor: "#ddd" },
+    "&.cm-focused .cm-selectionBackground, ::selection": { backgroundColor: "#353a44" },
+}, {dark: true});
+
+const myHighlightStyle = HighlightStyle.define([
+  {tag: tags.keyword, color: "#B4B4FD"},
+  {tag: tags.variableName, color: "#70CFFF"},
+  {tag: tags.namespace, color: "#78D454"},
+  {tag: tags.integer, color: "#32bd8f"},
+  {tag: tags.float, color: "#32bd8f"},
+  {tag: tags.bool, color: "#FB7478"},
+  {tag: tags.string, color: "#32bd8f"},
+  {tag: tags.comment, color: "#7ea2b4", fontStyle: "italic"},
+  {tag: tags.annotation, color: "#B4B4FD"},
+  {tag: tags.url, color: "#78D454"},
+  {tag: tags.typeName, color: "#FB7478"},
   
+])
+
 let view = new EditorView({
     doc: "# Enter your query here\n",
-    extensions:  [basicSetup, fixedHeightEditor, sparql()],
+    extensions:  [basicSetup, fixedHeightEditor, sparql(), syntaxHighlighting(myHighlightStyle)],
     parent: document.getElementById("wrapper") ?? undefined
 });
 
