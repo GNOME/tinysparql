f700250c55d30d9d78f0e1abf4c4392e47d6b5b6
Sam Thursfield <sam@afuera.me.uk>
2024-08-07 18:30:54 +0200

    webide: Identify the endpoint and database that we are connected to
    This is essential when you have more than one database in your life.
    
    When running the web IDE against my LocalSearch database, this currently
    shows the following text at the bottom left corner:
    
        Connected to endpoint: http://127.0.0.1:8080/sparql
        TinySPARQL database at /home/sam/.cache/tracker3/files/

--- src/web-ide/src/index.prod.ts
+++ src/web-ide/src/index.prod.ts
@@ -1,10 +1,15 @@
 import view from './editor';
-import { executeSparql } from './run';
+import { executeSparql, getEndpointDescriptor } from './run';
 import './style.scss';
 import './assets/favicon.ico';
 
+// For now the web IDE runs on the same port as the HTTP endpoint.
+// This will change once <https://gitlab.gnome.org/GNOME/tinysparql/-/issues/450>
+// is implemented.
+const endpointUrl = new URL("/sparql", window.location.href);
+
 document.getElementById("runBtn")?.addEventListener("click", async () => {
-    let { result, vars } = await executeSparql(String(view.state.doc), "/sparql");
+    let { result, vars } = await executeSparql(String(view.state.doc), endpointUrl.toString());
 
     // fill results section with error/results table etc.
     document.getElementById("right").replaceChildren(...result);
@@ -13,3 +18,21 @@ document.getElementById("runBtn")?.addEventListener("click", async () => {
     if (vars) document.getElementById("variable-selects").replaceChildren(...vars);
     else document.getElementById("variable-selects").innerHTML = "No variables in current query, use this to show or hide variables when there are.";
 });
+
+window.onload = async (event) => {
+    console.log("Connecting to endpoint to fetch service description.");
+
+    let connectionStatus = null;
+    try {
+        let descriptor = await getEndpointDescriptor(endpointUrl.toString());
+
+        let endpointName = descriptor["@graph"][0]["dc:title"];
+        document.getElementById("endpoint-name").innerHTML = endpointName;
+
+        connectionStatus = `Connected to endpoint: ${endpointUrl}`;
+    } catch (error) {
+        connectionStatus = `Failed to connect to ${endpointUrl}: ${error}`;
+    }
+
+    document.getElementById("connection-status").innerHTML = connectionStatus;
+}
