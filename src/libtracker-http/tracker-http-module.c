094a58764544c6899212c474dedd6dd116443444
Carlos Garnacho <carlosg@gnome.org>
2024-08-18 20:20:31 +0200

    http: Update user agent string
    Mention "tinysparql" in the name.

--- src/libtracker-http/tracker-http-module.c
+++ src/libtracker-http/tracker-http-module.c
@@ -94,7 +94,7 @@ static const gchar *mimetypes[] = {
 G_STATIC_ASSERT (G_N_ELEMENTS (mimetypes) == TRACKER_N_SERIALIZER_FORMATS);
 
 #define CONTENT_TYPE "application/sparql-query"
-#define USER_AGENT "Tracker " PACKAGE_VERSION " (https://gitlab.gnome.org/GNOME/tinysparql/issues/)"
+#define USER_AGENT "TinySPARQL " PACKAGE_VERSION " (https://gitlab.gnome.org/GNOME/tinysparql/issues/)"
 
 /* Server */
 struct _TrackerHttpRequest
