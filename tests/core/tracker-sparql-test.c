5c050fd7b1e3024d0736df7e2155aafc8574e2f8
Carlos Garnacho <carlosg@gnome.org>
2024-08-25 00:06:30 +0200

    tests: Revive Turtle base/prefix test
    This test was commented out in commit 38f244ff6b with the introduction
    of our deserialization APIs. We now paid our technical debt in the
    previous commits, so we can revive this test, and have it test prefix
    and base in a less indirect way.

--- tests/core/tracker-sparql-test.c
+++ tests/core/tracker-sparql-test.c
@@ -394,11 +394,9 @@ const TestInfo tests[] = {
 	{ "error/describe-1", "error/query-error-1", TRUE, FALSE },
 	{ "error/describe-2", "error/query-error-1", TRUE, FALSE },
 	{ "error/garbage-1", "error/query-error-1", TRUE, FALSE },
-
+	/* Turtle data */
 	{ "turtle/turtle-query-001", "turtle/turtle-data-001", FALSE },
-#if 0
 	{ "turtle/turtle-query-002", "turtle/turtle-data-002", FALSE },
-#endif
 	{ "turtle-comments/turtle-query-001", "turtle-comments/turtle-data-001", FALSE },
 	/* Mixed cardinality tests */
 	{ "mixed-cardinality/insert-mixed-cardinality-query-1", "mixed-cardinality/insert-mixed-cardinality-1", FALSE, FALSE },
