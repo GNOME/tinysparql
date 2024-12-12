cd2a49f0da6678779ec1818b703519537f27d9b9
Carlos Garnacho <carlosg@gnome.org>
2024-12-12 20:02:05 +0100

    docs: Use wrapper to gi-docgen
    As we need to "fix" files generated by gi-docgen (namely, the devhelp2
    and index.json files so they include our ontology keywords), do this as
    a wrapper script, instead of late as a install script.
    
    This allows us to use `install:` on the generated files like other
    gi-docgen users, instead of relying on our own install script.
    
    Related: https://gitlab.gnome.org/GNOME/tinysparql/-/issues/467

--- docs/reference/libtracker-sparql/dist-docs.sh
+++ docs/reference/libtracker-sparql/dist-docs.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-docs_name=$1
-srcdir=$2
-docs_path=$3
+docs_path=$1
 
 # Ensure the build tree is compiled, we need generated files
 ninja
-
-# Generate fixed .devhelp2 file
-. $srcdir/generate-devhelp.sh $docs_name $docs_path $srcdir
 
 # And copy the resulting devhelp documentation into the dist location
 cp -a $docs_path ${MESON_DIST_ROOT}/docs/reference/libtracker-sparql/
