GNOME/tinysparql

commit df4dc3276ba2f39af73b1b60262aabaa8e496c0c

cli: Avoid double free in "introspect" subcommand
If we get the class tree, the connection might be doubly freed
on error situations.

src/cli/tracker-introspect.c
@@ -716,7 +716,6 @@ tree_get (TrackerSparqlConnection *connection,
 		            _("Could not create tree: subclass query failed"),
 		            error->message);
 		g_error_free (error);
-		g_object_unref (connection);
 		g_free (class_lookup_longhand);
 
 		return EXIT_FAILURE;

commit 218272220a1eaa81a99f6d9f1fae7a461a21e6e7

cli: Avoid another possible double free on error
Missed in the previous fixups.

Fixes: df4dc3276 ("cli: Avoid double free in "introspect" subcommand")

src/cli/tracker-introspect.c
@@ -765,7 +765,6 @@ tree_get (TrackerSparqlConnection *connection,
 			            _("Could not create tree: class properties query failed"),
 			            error->message);
 			g_error_free (error);
-			g_object_unref (connection);
 
 			g_free (class_lookup_longhand);
 