82d22240a21c20371c5c89c8af9d04b24ea6e346
Carlos Garnacho <carlosg@gnome.org>
2025-03-21 11:15:59 +0100

    docs: Fix path to copy pre-generated docs for dist tarball
    It broke in the shuffling of our documentation.
    
    Fixes: 77ccd9b33 ("build: Rename "docs/reference/libtracker-sparql" to "docs/reference"")
    Closes: https://gitlab.gnome.org/GNOME/tinysparql/-/issues/483

--- docs/reference/dist-docs.sh
+++ docs/reference/dist-docs.sh
@@ -5,4 +5,4 @@ docs_path=$1
 ninja
 
 # And copy the resulting devhelp documentation into the dist location
-cp -a $docs_path ${MESON_DIST_ROOT}/docs/reference/libtracker-sparql/
+cp -a $docs_path ${MESON_DIST_ROOT}/docs/reference/doc/
