GNOME/tinysparql

commit 1f3019a4837a0f99dd0d436ccd639947ad126bf5

bus: Cancel TrackerNotifier ongoing operation when detaching
The TrackerNotifier might be caught at the moment of querying extra
information when the SPARQL connection is closed. This could result in
possible warnings.

src/libtinysparql/bus/tracker-bus.c
@@ -31,6 +31,7 @@
 #include "tracker-bus-batch.h"
 #include "tracker-bus-cursor.h"
 #include "tracker-bus-statement.h"
+#include "tracker-notifier-private.h"
 
 #define DBUS_PEER_IFACE "org.freedesktop.DBus.Peer"
 
@@ -47,6 +48,7 @@ struct _TrackerBusConnection {
 
 	GDBusConnection *dbus_conn;
 	TrackerNamespaceManager *namespaces;
+	GList *notifiers;
 	gchar *dbus_name;
 	gchar *object_path;
 	gboolean sandboxed;
@@ -1251,13 +1253,15 @@ tracker_bus_connection_create_notifier (TrackerSparqlConnection *self)
 
 	notifier = g_object_new (TRACKER_TYPE_NOTIFIER,
 	                         "connection", self,
-				 NULL);
+	                         NULL);
 
 	tracker_notifier_signal_subscribe (notifier,
 	                                   bus->dbus_conn,
 	                                   bus->dbus_name,
 	                                   bus->object_path,
 	                                   NULL);
+
+	bus->notifiers = g_list_prepend (bus->notifiers, notifier);
 
 	return notifier;
 }
@@ -1266,6 +1270,12 @@ static void
 tracker_bus_connection_close (TrackerSparqlConnection *self)
 {
 	TrackerBusConnection *bus = TRACKER_BUS_CONNECTION (self);
+
+	while (bus->notifiers) {
+		tracker_notifier_stop (bus->notifiers->data);
+		bus->notifiers = g_list_remove (bus->notifiers,
+		                                bus->notifiers->data);
+	}
 
 	if (bus->sandboxed) {
 		GDBusMessage *message;

commit c349e8edbd4dff576b7c69945fbf28388e2f2f2b

bus: Track bus connection notifiers through weak refs
We added some accounting for these at commit 1f3019a4837, but
forgot that the notifiers are not owned by the connection. This
means we have to track them through weak references like the
direct backend does, and drop them from the list once the user
disposed of them.

Fixes: 1f3019a48 ("bus: Cancel TrackerNotifier ongoing operation when detaching")

src/libtinysparql/bus/tracker-bus.c
@@ -660,9 +660,34 @@ tracker_bus_connection_async_initable_iface_init (GAsyncInitableIface *iface)
 }
 
 static void
+notifier_weak_ref_cb (gpointer  data,
+                      GObject  *prev_location)
+{
+	TrackerBusConnection *bus = data;
+
+	bus->notifiers = g_list_remove (bus->notifiers, prev_location);
+}
+
+static void
+clear_notifiers (TrackerBusConnection *bus)
+{
+	while (bus->notifiers) {
+		TrackerNotifier *notifier = bus->notifiers->data;
+
+		tracker_notifier_stop (notifier);
+		g_object_weak_unref (G_OBJECT (notifier),
+		                     notifier_weak_ref_cb,
+		                     bus);
+		bus->notifiers = g_list_remove (bus->notifiers, notifier);
+	}
+}
+
+static void
 tracker_bus_connection_finalize (GObject *object)
 {
 	TrackerBusConnection *bus = TRACKER_BUS_CONNECTION (object);
+
+	clear_notifiers (bus);
 
 	g_clear_object (&bus->dbus_conn);
 	g_clear_pointer (&bus->dbus_name, g_free);
@@ -1261,6 +1286,7 @@ tracker_bus_connection_create_notifier (TrackerSparqlConnection *self)
 	                                   bus->object_path,
 	                                   NULL);
 
+	g_object_weak_ref (G_OBJECT (notifier), notifier_weak_ref_cb, self);
 	bus->notifiers = g_list_prepend (bus->notifiers, notifier);
 
 	return notifier;
@@ -1271,11 +1297,7 @@ tracker_bus_connection_close (TrackerSparqlConnection *self)
 {
 	TrackerBusConnection *bus = TRACKER_BUS_CONNECTION (self);
 
-	while (bus->notifiers) {
-		tracker_notifier_stop (bus->notifiers->data);
-		bus->notifiers = g_list_remove (bus->notifiers,
-		                                bus->notifiers->data);
-	}
+	clear_notifiers (bus);
 
 	if (bus->sandboxed) {
 		GDBusMessage *message;
@@ -1299,6 +1321,8 @@ tracker_bus_connection_close_async (TrackerSparqlConnection *connection,
 	GTask *task;
 
 	task = g_task_new (connection, cancellable, callback, user_data);
+
+	clear_notifiers (bus);
 
 	if (bus->sandboxed) {
 		GDBusMessage *message;