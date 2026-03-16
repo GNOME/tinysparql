GNOME/tinysparql

commit f681f4626408b96a74621e38e5162cd2e3f8e083

direct: Improve locking around TrackerNotifier creation
This may be called at unfortunate times (e.g. when the connection is
being closed from another thread), which might result in the notifier
attempting to attach itself to a TrackerDataManager that is gone, or
going away.

Coverity was complaining that about missing locking during
tracker_data_finalize() when dropping the update callbacks, fix that
and improve the handling of the direct connection's create_notifier()
vmethod, so that it is guaranteed to return a notifier that is either
attached to the TrackerDataManager (thus going away when it closes),
or disconnected from the get go (thus inert), without in betweens.

We cannot reliably test the race condition itself, so at least ensure
in tests that a notifier can be safely created after closing the
connection.

CIDs: #1668772, #1668773

src/libtinysparql/direct/tracker-direct.c
@@ -49,6 +49,7 @@ struct _TrackerDirectConnectionPrivate
 	GThreadPool *select_pool;
 
 	GList *notifiers;
+	GMutex notifiers_mutex;
 
 	gint64 timestamp;
 	gint64 cleanup_timestamp;
@@ -839,8 +840,12 @@ weak_ref_notify (gpointer  data,
                  GObject  *prev_location)
 {
 	TrackerDirectConnection *conn = data;
+	TrackerDirectConnectionPrivate *priv =
+		tracker_direct_connection_get_instance_private (conn);
 
+	g_mutex_lock (&priv->notifiers_mutex);
 	detach_notifier (conn, (TrackerNotifier *) prev_location);
+	g_mutex_unlock (&priv->notifiers_mutex);
 }
 
 static void
@@ -859,6 +864,8 @@ tracker_direct_connection_finalize (GObject *object)
 	g_clear_object (&priv->ontology);
 	g_clear_object (&priv->namespace_manager);
 	g_clear_object (&priv->ontology_rdf);
+	g_mutex_clear (&priv->update_mutex);
+	g_mutex_clear (&priv->notifiers_mutex);
 
 	G_OBJECT_CLASS (tracker_direct_connection_parent_class)->finalize (object);
 }
@@ -1215,14 +1222,20 @@ tracker_direct_connection_create_notifier (TrackerSparqlConnection *self)
 	                         "connection", self,
 				 NULL);
 
-	tracker_data = tracker_data_manager_get_data (priv->data_manager);
-	tracker_data_add_callbacks (tracker_data,
-	                            statement_cb,
-	                            transaction_cb,
-	                            notifier);
+	g_mutex_lock (&priv->notifiers_mutex);
 
-	g_object_weak_ref (G_OBJECT (notifier), weak_ref_notify, self);
-	priv->notifiers = g_list_prepend (priv->notifiers, notifier);
+	if (!priv->closing) {
+		tracker_data = tracker_data_manager_get_data (priv->data_manager);
+		tracker_data_add_callbacks (tracker_data,
+		                            statement_cb,
+		                            transaction_cb,
+		                            notifier);
+
+		g_object_weak_ref (G_OBJECT (notifier), weak_ref_notify, self);
+		priv->notifiers = g_list_prepend (priv->notifiers, notifier);
+	}
+
+	g_mutex_unlock (&priv->notifiers_mutex);
 
 	return notifier;
 }
@@ -1252,6 +1265,8 @@ tracker_direct_connection_close (TrackerSparqlConnection *self)
 		priv->select_pool = NULL;
 	}
 
+	g_mutex_lock (&priv->notifiers_mutex);
+
 	while (priv->notifiers) {
 		TrackerNotifier *notifier = priv->notifiers->data;
 
@@ -1260,6 +1275,8 @@ tracker_direct_connection_close (TrackerSparqlConnection *self)
 		                     conn);
 		detach_notifier (conn, notifier);
 	}
+
+	g_mutex_unlock (&priv->notifiers_mutex);
 
 	if (priv->data_manager) {
 		tracker_data_manager_shutdown (priv->data_manager);

src/libtinysparql/core/tracker-data-update.c
@@ -623,11 +623,12 @@ tracker_data_finalize (GObject *object)
 	g_clear_object (&data->update_buffer.insert_resource);
 	tracker_db_statement_mru_finish (&data->update_buffer.stmt_mru);
 
-	g_clear_pointer (&data->statement_callbacks, g_ptr_array_unref);
-	g_clear_pointer (&data->transaction_callbacks, g_ptr_array_unref);
-
 	g_clear_pointer (&data->tz, g_time_zone_unref);
 
+	g_mutex_lock (&data->callbacks_lock);
+	g_clear_pointer (&data->statement_callbacks, g_ptr_array_unref);
+	g_clear_pointer (&data->transaction_callbacks, g_ptr_array_unref);
+	g_mutex_unlock (&data->callbacks_lock);
 	g_mutex_clear (&data->callbacks_lock);
 
 	G_OBJECT_CLASS (tracker_data_parent_class)->finalize (object);

tests/libtinysparql/tracker-connection-test.c
@@ -169,6 +169,7 @@ assert_operations_after_close (TrackerSparqlConnection *conn)
 	TrackerResource *resource;
 	GError *error = NULL;
 	GMainLoop *main_loop;
+	TrackerNotifier *notifier;
 
 	resource = tracker_resource_new ("file:///");
 	tracker_resource_set_uri (resource, "rdf:type", "rdfs:Resource");
@@ -201,6 +202,10 @@ assert_operations_after_close (TrackerSparqlConnection *conn)
 
 		tracker_sparql_connection_update_async (conn, "", NULL, update_closed_cb, main_loop);
 		g_main_loop_run (main_loop);
+
+		notifier = tracker_sparql_connection_create_notifier (conn);
+		g_assert_nonnull (notifier);
+		g_object_unref (notifier);
 	}
 
 	g_clear_object (&resource);

commit b139706196da17089dbd0b5ee0f8713d1d50264d

direct: Initialize mutexes
For some reason, these don't seem to be mandatory on Linux, but
are reported to trigger crashes on MacOS now that g_mutex_clear()
is called on these during finalization. Add the missing g_mutex_init()
calls so mutex lifetime is correct.

Fixes: f681f4626 ("direct: Improve locking around TrackerNotifier creation")
Closes: https://gitlab.gnome.org/GNOME/tinysparql/-/issues/497

src/libtinysparql/direct/tracker-direct.c
@@ -699,6 +699,11 @@ tracker_direct_connection_async_initable_iface_init (GAsyncInitableIface *iface)
 static void
 tracker_direct_connection_init (TrackerDirectConnection *conn)
 {
+	TrackerDirectConnectionPrivate *priv =
+		tracker_direct_connection_get_instance_private (conn);
+
+	g_mutex_init (&priv->update_mutex);
+	g_mutex_init (&priv->notifiers_mutex);
 }
 
 static GHashTable *